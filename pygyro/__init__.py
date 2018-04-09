08f57846a7ccc00efa911fe1590d13bb0adb456c
ebourne <louise.bourne@gmail.com>
2018-04-09 18:42:41 +0200

    Modified init to make specific files accessible from multiple folders

---
+++ pygyro/__init__.py
@@ -0,0 +1,13 @@
+
+from .Core.Grid import *
+from .initialisation.Setups import *
+import src.initialisation.Constants as Constants
+from .Utilities.GridPlotter import *
+from splines import *
+
+
+# tests
+from .initialisation.TestSetup import *
+from .Core.TestCore import *
+from .Utilities.TestPlots import *
+
