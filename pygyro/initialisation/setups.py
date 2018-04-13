26f9effe2360afebed236253ed75f9450db72b89
ebourne <louise.bourne@gmail.com>
2018-04-13 16:15:26 +0200

    Changed setup method. "setup_grid" calculates grid points but Grid calculates splitting. The returned value is then used in the initialise function

--- pygyro/initialisation/setups.py
+++ pygyro/initialisation/setups.py
@@ -1,13 +1,12 @@
-from mpi4py import MPI
 from math import pi
 import numpy as np
 
 from ..           import splines as spl
-from ..model.grid import Grid
+from ..model.grid import Grid, Layout
 from .initialiser import initialise
+from .            import constants
 
-def RadialSetup(nr: int, ntheta: int, nz: int, nv: int, rMin: float,
-                rMax: float, zMin: float, zMax: float, vMax: float, vMin: float = None,m=None,n=None):
+def setupGrid(nr: int, ntheta: int, nz: int, nv: int, layout: Layout, **kwargs):
     """
     Setup using radial topology can be initialised using the following arguments:
     
@@ -16,25 +15,30 @@ def RadialSetup(nr: int, ntheta: int, nz: int, nv: int, rMin: float,
     ntheta -- number of points in the tangential direction
     nz     -- number of points in the axial direction
     nv     -- number of velocities for v perpendicular
-    rMin   -- minimum radius, a float
-    rMax   -- maximum radius, a float
-    zMin   -- minimum value in the z direction, a float
-    zMax   -- maximum value in the z direction, a float
-    vMax   -- maximum velocity, a float
+    layout -- parallel distribution configuration
     
     Optional arguments:
-    vMin -- minimum velocity, a float. (default -vMax)
+    rMin   -- minimum radius, a float. (default constants.rMin)
+    rMax   -- maximum radius, a float. (default constants.rMax)
+    zMin   -- minimum value in the z direction, a float. (default constants.zMin)
+    zMax   -- maximum value in the z direction, a float. (default constants.zMax)
+    vMax   -- maximum velocity, a float. (default constants.vMax)
+    vMin   -- minimum velocity, a float. (default -vMax)
     
-    >>> RadialSetup(rMin = float,rMax = float,zMin = float,float
+    >>> setupGrid(256,512,32,128,Layout.FIELD_ALIGNED)
     """
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-    
-    if (vMin==None):
-        vMin=-vMax
-    elif (not isinstance(vMin,float)):
-        raise TypeError("Wrong type for vMin in Radial Setup")
+    rMin=kwargs.pop('rMin',constants.rMin)
+    rMax=kwargs.pop('rMin',constants.rMax)
+    zMin=kwargs.pop('rMin',constants.zMin)
+    zMax=kwargs.pop('rMin',constants.zMax)
+    vMax=kwargs.pop('rMin',constants.vMax)
+    vMin=kwargs.pop('rMin',-vMax)
+    m=kwargs.pop('m',constants.m)
+    n=kwargs.pop('n',constants.n)
+    degree=kwargs.pop('degree',3)
+    nProcR=kwargs.pop('nProcR',1)
+    nProcZ=kwargs.pop('nProcZ',1)
+    nProcV=kwargs.pop('nProcV',1)
     
     degree=3
     
@@ -56,87 +60,6 @@ def RadialSetup(nr: int, ntheta: int, nz: int, nv: int, rMin: float,
     zVals = zSpline.greville
     vVals = vSpline.greville
     
-    nrOverflow=nr%size
-    nzOverflow=nz%size
-    ranks=np.arange(0,size)
-    rStarts=nr//size*ranks + np.minimum(ranks,nrOverflow)
-    zStarts=nz//size*ranks + np.minimum(ranks,nzOverflow)
-    rStarts=np.append(rStarts,len(rVals))
-    zStarts=np.append(zStarts,len(zVals))
-    rLen=nr//size+np.less(ranks,nrOverflow).astype(int)
-    zLen=nz//size+np.less(ranks,nzOverflow).astype(int)
-    rEnd=rStarts[rank]+rLen[rank]
-    
-    # ordering chosen to increase step size to improve cache-coherency
-    f = np.empty((ntheta,rLen[rank],nz,nv),float,order='F')
-    
-    initialise(f,rVals[rStarts[rank]:rEnd],qVals,zVals,vVals,m,n)
-    return Grid(rVals,rSpline,qVals,qSpline,zVals,zSpline,vVals,vSpline,rStarts,zStarts,f,"radial")
-
-
-def BlockSetup(nr: int, ntheta: int, nz: int, nv: int, rMin: float,
-                rMax: float, zMin: float, zMax: float, vMax: float, vMin: float = None,m=None,n=None):
-    """
-    Setup using topology split in the z direction can be initialised using the following arguments:
-    
-    Compulsory arguments:
-    nr     -- number of points in the radial direction
-    ntheta -- number of points in the tangential direction
-    nz     -- number of points in the axial direction
-    nv     -- number of velocities for v perpendicular
-    rMin   -- minimum radius, a float
-    rMax   -- maximum radius, a float
-    zMin   -- minimum value in the z direction, a float
-    zMax   -- maximum value in the z direction, a float
-    vMax   -- maximum velocity, a float
-    
-    Optional arguments:
-    vMin -- minimum velocity, a float. (default -vMax)
-    
-    >>> BlockSetup(rMin = float,rMax = float,zMin = float,float
-    """
-        
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-    if (vMin==None):
-        vMin=-vMax
-    elif (not isinstance(vMin,float)):
-        raise TypeError("Wrong type for vMin in Radial Setup")
-    
-    degree=3
-    
-    # get spline knots
-    rKnots = spl.make_knots(np.linspace(rMin,rMax,nr-degree+1),degree,False)
-    qKnots = spl.make_knots(np.linspace(0,2*pi,ntheta+1),degree,True)
-    zKnots = spl.make_knots(np.linspace(zMin,zMax,nz+1),degree,True)
-    vKnots = spl.make_knots(np.linspace(vMin,vMax,nv-degree+1),degree,False)
-    
-    # make splines
-    rSpline = spl.BSplines(rKnots,degree,False)
-    qSpline = spl.BSplines(qKnots,degree,True)
-    zSpline = spl.BSplines(zKnots,degree,True)
-    vSpline = spl.BSplines(vKnots,degree,False)
-    
-    # get greville points
-    rVals = rSpline.greville
-    qVals = qSpline.greville
-    zVals = zSpline.greville
-    vVals = vSpline.greville
-    
-    nrOverflow=nr%size
-    nzOverflow=nz%size
-    ranks=np.arange(0,size)
-    rStarts=nr//size*ranks+np.minimum(ranks,nrOverflow)
-    zStarts=nz//size*ranks+np.minimum(ranks,nzOverflow)
-    rStarts=np.append(rStarts,len(rVals))
-    zStarts=np.append(zStarts,len(zVals))
-    rLen=nr//size+np.less(ranks,nrOverflow).astype(int)
-    zLen=nz//size+np.less(ranks,nzOverflow).astype(int)
-    zEnd=zStarts[rank]+zLen[rank]
-    
-    # ordering chosen to increase step size to improve cache-coherency
-    f = np.empty((ntheta,nr,zLen[rank],nv),float,order='F')
-    
-    initialise(f,rVals,qVals,zVals[zStarts[rank]:zEnd],vVals,m,n)
-    return Grid(rVals,rSpline,qVals,qSpline,zVals,zSpline,vVals,vSpline,rStarts,zStarts,f,"block")
+    grid=Grid(rVals,rSpline,qVals,qSpline,zVals,zSpline,vVals,vSpline,layout,nProcR=nProcR,nProcZ=nProcZ,nProcV=nProcV)
+    initialise(grid.f,grid.getRCoords(),grid.getThetaCoords(),grid.getZCoords(),grid.getVCoords(),m,n)
+    return grid
