1f8b0a8ef12d9a542e944280454d33d5b6bef7ec
ebourne <louise.bourne@gmail.com>
2018-04-17 15:37:55 +0200

    Removed splines from grid (currently unused, may well remain so as 2D splines will be used. Made grid tests independant from rest of project. Added a test for wrong processor arangement

--- pygyro/model/test_grid.py
+++ pygyro/model/test_grid.py
@@ -1,9 +1,9 @@
 from mpi4py import MPI
 import numpy as np
 import pytest
+from math import pi
 
 from .grid import Layout, Grid
-from ..initialisation.setups import setupGrid
 
 def define_f(rVals,qVals,zVals,vVals,grid):
     nr=len(rVals)
@@ -37,10 +37,36 @@ def compare_f(rVals,qVals,zVals,vVals,grid):
 
 @pytest.mark.serial
 def test_Grid():
-    Grid([0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],Layout.FIELD_ALIGNED,nProcR=2,nProcV=2)
-    Grid([0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],Layout.POLOIDAL,nProcV=2,nProcZ=2)
+    Grid([0,1],[0,1],[0,1],[0,1],Layout.FIELD_ALIGNED,nProcR=2,nProcV=2)
+    Grid([0,1],[0,1],[0,1],[0,1],Layout.POLOIDAL,nProcV=2,nProcZ=2)
     with pytest.raises(NotImplementedError):
-        Grid([0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],"madeup")
+        Grid([0,1],[0,1],[0,1],[0,1],"madeup")
+
+@pytest.mark.parallel
+def test_ErrorsRaised():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    if (size==1):
+        return
+    i=2
+    while (i<size):
+        n1=i
+        n2=size//i
+        if (n1*n2==size):
+            i=size
+        else:
+            i=i+1
+    
+    if (n1*n2!=size):
+        return
+    else:
+        with pytest.raises(ValueError):
+            Grid([0,1],[0,1],[0,1],[0,1],Layout.FIELD_ALIGNED,nProcR=2,nProcZ=2)
+        with pytest.raises(ValueError):
+            Grid([0,1],[0,1],[0,1],[0,1],Layout.POLOIDAL,nProcR=2,nProcZ=2)
+        with pytest.raises(ValueError):
+            Grid([0,1],[0,1],[0,1],[0,1],Layout.V_PARALLEL,nProcR=2,nProcV=2)
 
 @pytest.mark.parallel
 @pytest.mark.parametrize("splitN", [1,2,3,4,5,6])
@@ -58,7 +84,9 @@ def test_layoutSwap(splitN):
     nr=10
     nz=15
     
-    grid = setupGrid(nr,ntheta,nz,nv,Layout.FIELD_ALIGNED,m=15,n=20,nProcR=n1,nProcV=n2)
+    #grid = setupGrid(nr,ntheta,nz,nv,Layout.FIELD_ALIGNED,nProcR=n1,nProcV=n2)
+    grid = Grid(np.linspace(0.5,14.5,nr),np.linspace(0,2*pi,ntheta,endpoint=False),
+                np.linspace(0,50,nz),np.linspace(-5,5,nv),Layout.FIELD_ALIGNED,nProcR=n1,nProcV=n2)
     oldShape = grid.f.shape
     define_f(grid.Vals[Grid.Dimension.R],grid.Vals[Grid.Dimension.THETA],
             grid.Vals[Grid.Dimension.Z],grid.Vals[Grid.Dimension.V],grid)
