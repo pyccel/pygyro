c76a8199334c8f7013d24b5bb0c63c8523a70f09
ebourne <louise.bourne@gmail.com>
2018-05-09 16:19:28 +0200

    Fixed 4D plotting

--- pygyro/model/grid.py
+++ pygyro/model/grid.py
@@ -101,7 +101,7 @@ class Grid(object):
     ####################################################################
     
     
-    def getSliceFromDict(self,d : dict):
+    def getSliceFromDict(self,d : dict, comm: MPI.Comm, rank: int):
         """
         Utility function to access getSliceForFig without using 6 if statements
         The function takes a dictionary which plots the fixed dimensions to the
@@ -110,12 +110,12 @@ class Grid(object):
         >>> getSliceFromDict({self.Dimension.ETA3: 5, self.Dimension.ETA4 : 2})
         """
         dim1 = d.get(self._layout.dims_order[0],None)
-        dim2 = d.get(self.Dimension.ETA2,None)
-        dim3 = d.get(self.Dimension.ETA3,None)
-        dim4 = d.get(self.Dimension.ETA4,None)
-        return self.getSliceForFig(eta1,eta2,eta3,eta4)
+        dim2 = d.get(self._layout.dims_order[1],None)
+        dim3 = d.get(self._layout.dims_order[2],None)
+        dim4 = d.get(self._layout.dims_order[3],None)
+        return self.getSliceForFig([dim1,dim2,dim3,dim4],comm,rank)
     
-    def getSliceForFig(self,dim1 = None,dim2 = None,dim3 = None,dim4 = None):
+    def getSliceForFig(self, dims: list, comm: MPI.Comm, rank: int):
         """
         Class to retrieve a 2D slice. Any values set to None will vary.
         Any dimensions not set to None will be fixed at the global index provided
@@ -123,7 +123,8 @@ class Grid(object):
         
         # helper variables to correctly reshape the slice after gathering
         dimSize=[]
-        dimIdx=[]
+        dim_slices = []
+        shape = []
         
         # Get slices for each dimension (eta2_slice, eta1_slice, eta3_slice, eta4_slice)
         # If value is None then all values along that dimension should be returned
@@ -132,128 +133,87 @@ class Grid(object):
         # if that index cannot be found on the current process then None will
         # be stored
         
-        if (dim1==None):
-            dim1_slice=slice(0,self.dim1_end-self.dim1_start)
-            dimSize.append(self.nEta1)
-            dimIdx.append(self.Dimension.ETA1)
-        else:
-            dim1_slice=None
-            if (dim1>=self.dim1_start and dim1<self.dim1_end):
-                dim1_slice=dim1-self.dim1_start
-        if (dim3==None):
-            dim3_slice=slice(0,self.dim3_end-self.dim3_start)
-            dimSize.append(self.nEta3)
-            dimIdx.append(self.Dimension.ETA3)
-        else:
-            dim3_slice=None
-            if (dim3>=self.dim3_start and dim3<self.dim3_end):
-                dim3_slice=dim3-self.dim3_start
-        if (dim4==None):
-            dim4_slice=slice(0,self.dim4_end-self.dim4_start)
-            dimSize.append(self.nEta4)
-            dimIdx.append(self.Dimension.ETA4)
-        else:
-            dim4_slice=None
-            if (dim4>=self.dim4_start and dim4<self.dim4_end):
-                dim4_slice=dim4-self.dim4_start
-        if (dim2==None):
-            dim2_slice=slice(0,self.nEta2)
-            dimSize.append(self.nEta2)
-            dimIdx.append(self.Dimension.ETA2)
-        else:
-            dim2_slice=dim2
+        for i,dim_i in enumerate(dims):
+            if (dim_i==None):
+                dim_slices.append(slice(0,self._layout.ends[i]-self._layout.starts[i]))
+                dimSize.append(self._layout.ends[i]-self._layout.starts[i])
+            else:
+                if (dim_i>=self._layout.starts[i] and dim_i<self._layout.ends[i]):
+                    dim_slices.append(dim_i-self._layout.starts[i])
+                    dimSize.append(1)
+                else:
+                    dim_slices.append(None)
+                    dimSize.append(None)
         
         # if the data is not on this process then at least one of the slices is equal to None
         # in this case send something of size 0
-        if (None in [eta1_slice,eta3_slice,eta4_slice,eta2_slice]):
-            sendSize=0
+        sendInfo = self._layout_manager.mpiCoords
+        if (None in dim_slices):
             toSend = np.ndarray(0)
+            sendInfo.append(0)
         else:
             # set sendSize and data to be sent
-            toSend = self.f[eta1_slice,eta3_slice,eta4_slice,eta2_slice].flatten()
-            sendSize=toSend.size
+            toSend = self._f[dim_slices].flatten()
+            sendInfo.append(toSend.size)
         
-        # collect the send sizes to properly formulate the gatherv
-        sizes=self.commEta14.gather(sendSize,root=0)
+        mpi_data=comm.gather(sendInfo,root=rank)
         
-        # the dimensions must be concatenated one at a time to properly shape the output.
-        if (self.rankEta14==0):
+        if (comm.Get_rank()==rank):
+            sizes = [coords.pop() for coords in mpi_data]
             # use sizes to get start points
             starts=np.zeros(len(sizes),int)
-            starts[1:]=np.cumsum(sizes[:self.sizeEta14-1])
+            starts[1:]=np.cumsum(sizes[:comm.Get_size()-1])
             
             # save memory for gatherv to fill
-            mySlice = np.empty(np.sum(sizes), dtype=float)
+            sliceSize = np.sum(sizes)
+            mySlice = np.empty(sliceSize, dtype=float)
             
-            # Gather information from all ranks to rank 0 in direction eta1 or eta4 (dependant upon layout)
-            self.commEta14.Gatherv(toSend,(mySlice, sizes, starts, MPI.DOUBLE), 0)
-            
-            # get dimensions along which split occurs
-            if (self.layout==Layout.FIELD_ALIGNED):
-                splitDims=[self.Dimension.ETA1, self.Dimension.ETA4]
-            elif (self.layout==Layout.POLOIDAL):
-                splitDims=[self.Dimension.ETA4, self.Dimension.ETA3]
-            elif (self.layout==Layout.V_PARALLEL):
-                splitDims=[self.Dimension.ETA1, self.Dimension.ETA3]
-            
-            # if the first dimension of the slice is distributed
-            if (splitDims[0]==dimIdx[0]):
-                # break received data into constituent parts
-                mySlice=np.split(mySlice,starts[1:])
-                for i in range(0,self.sizeEta14):
-                    # return them to original shape
-                    mySlice[i]=mySlice[i].reshape(sizes[i]//dimSize[1],dimSize[1])
-                # stitch back together along broken axis
-                mySlice=np.concatenate(mySlice,axis=0)
-            # if the second dimension of the slice is distributed
-            elif(splitDims[0]==dimIdx[1]):
-                # break received data into constituent parts
-                mySlice=np.split(mySlice,starts[1:])
-                for i in range(0,self.sizeEta14):
-                    # return them to original shape
-                    mySlice[i]=mySlice[i].reshape(dimSize[0],sizes[i]//dimSize[0])
-                # stitch back together along broken axis
-                mySlice=np.concatenate(mySlice,axis=1)
-            # else the data will be sent flattened anyway
-            
-            sizes=self.commEta34.gather(mySlice.size,root=0)
-            if (self.rankEta34==0):
-                # use sizes to get new start points
-                starts=np.zeros(len(sizes),int)
-                starts[1:]=np.cumsum(sizes[:self.sizeEta34-1])
-                
-                # ensure toSend has correct size in memory for gatherv to fill
-                toSend = np.empty(np.sum(sizes), dtype=float)
-                
-                # collect data sent up first column on first cell
-                self.commEta34.Gatherv(mySlice,(toSend, sizes, starts, MPI.DOUBLE), 0)
-                
-                # if the first dimension of the slice is distributed
-                if (splitDims[1]==dimIdx[0]):
-                    # break received data into constituent parts
-                    mySlice=np.split(toSend,starts[1:])
-                    for i in range(0,self.sizeEta34):
-                        # return them to original shape
-                        mySlice[i]=mySlice[i].reshape(sizes[i]//dimSize[1],dimSize[1])
-                    # stitch back together along broken axis
-                    return np.concatenate(mySlice,axis=0)
-                # if the second dimension of the slice is distributed
-                elif(splitDims[1]==dimIdx[1]):
-                    # break received data into constituent parts
-                    mySlice=np.split(toSend,starts[1:])
-                    for i in range(0,self.sizeEta34):
-                        # return them to original shape
-                        mySlice[i]=mySlice[i].reshape(dimSize[0],sizes[i]//dimSize[0])
-                    # stitch back together along broken axis
-                    return np.concatenate(mySlice,axis=1)
-                # if neither dimension is distributed
-                else:
-                    # ensure that the data has the right return shape
-                    mySlice=toSend.reshape(dimSize[0],dimSize[1])
-                return mySlice
-            else:
-                # send data collected to first column up to first cell
-                self.commEta34.Gatherv(mySlice,mySlice, 0)
+            # Gather information from all ranks to rank 0 in the
+            # direction of the comm
+            comm.Gatherv(toSend,(mySlice, sizes, starts, MPI.DOUBLE), 0)
+            return (self._layout,starts,mpi_data,self._layout_manager.nProcs,mySlice)
         else:
             # Gather information from all ranks
-            self.commEta14.Gatherv(toSend,toSend, 0)
+            comm.Gatherv(toSend,toSend, 0)
+    
+    def getMin(self,axis = None,fixValue = None):
+        
+        # if we want the total of all points on the grid
+        if (axis==None and fixValue==None):
+            # return the min of the min found on each process
+            return self.global_comm.reduce(np.amin(self._f),op=MPI.MIN,root=0)
+        
+        # if we want the total of all points on a (N-1)D slice where the
+        # value of eta_i is fixed ensure that the required index is
+        # covered by this process
+        dim = self._layout.inv_dims_order[axis]
+        if (fixValue>=self._layout.starts[dim] and 
+            fixValue<self._layout.ends[dim]):
+            idx = (np.s_[:],) * dim + (fixValue-self._layout.starts[dim],)
+            return self.global_comm.reduce(np.amin(self._f[idx]),op=MPI.MIN,root=0)
+        
+        # if the data is not on this process then send the largest possible value of f
+        # this way min will always choose an alternative
+        else:
+            return self.global_comm.reduce(1,op=MPI.MIN,root=0)
+    
+    def getMax(self,axis = None,fixValue = None):
+        
+        # if we want the total of all points on the grid
+        if (axis==None and fixValue==None):
+            # return the max of the max found on each process
+            return self.global_comm.reduce(np.amax(self._f),op=MPI.MAX,root=0)
+        
+        # if we want the total of all points on a (N-1)D slice where the
+        # value of eta_i is fixed ensure that the required index is
+        # covered by this process
+        dim = self._layout.inv_dims_order[axis]
+        if (fixValue>=self._layout.starts[dim] and 
+            fixValue<self._layout.ends[dim]):
+            idx = (np.s_[:],) * dim + (fixValue-self._layout.starts[dim],)
+            return self.global_comm.reduce(np.amax(self._f[idx]),op=MPI.MAX,root=0)
+        
+        # if the data is not on this process then send the smallest possible value of f
+        # this way max will always choose an alternative
+        else:
+            return self.global_comm.reduce(0,op=MPI.MAX,root=0)
