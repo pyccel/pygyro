c20c95e856432cb4b46cc47f8e26bad3687c3f8e
ebourne <louise.bourne@gmail.com>
2018-05-23 17:57:17 +0200

    Corrected to use 1D splines instead of 2D. Don't forget to ensure degree 5 polynomial for an integer step

--- pygyro/advection/advection.py
+++ pygyro/advection/advection.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.interpolate              import lagrange
-from math                           import pi
+from math                           import pi, floor, ceil
 
 from ..splines.splines              import Spline1D, Spline2D
 from ..splines.spline_interpolators import SplineInterpolator1D, SplineInterpolator2D
@@ -20,8 +20,8 @@ class fluxSurfaceAdvection:
     def __init__( self, eta_grid, splines, iota = constants.iota ):
         self._points = eta_grid[1:3]
         self._nPoints = (self._points[0].size,self._points[1].size)
-        self._interpolator = SplineInterpolator2D(splines[0],splines[1])
-        self._spline = Spline2D(splines[0],splines[1])
+        self._interpolator = SplineInterpolator1D(splines[0])
+        self._thetaSplines = [Spline1D(splines[0]) for i in range(self._nPoints[1])]
         self._dz = eta_grid[2][1]-eta_grid[2][0]
         try:
             self._dtheta =  np.atleast_2d(self._dz * iota() / constants.R0)
@@ -29,40 +29,35 @@ class fluxSurfaceAdvection:
             self._dtheta = np.atleast_2d(self._dz * iota(eta_grid[0]) / constants.R0).T
         
         self._bz = self._dz / np.sqrt(self._dz**2+self._dtheta**2)
-        
-        self._thetaStep = np.mod(self._points[0]+self._dtheta,2*pi)
-        self._theta2Step = np.mod(self._points[0]+2*self._dtheta,2*pi)
-        self._theta3Step = np.mod(self._points[0]+3*self._dtheta,2*pi)
-        self._thetaBStep = np.mod(self._points[0]-self._dtheta,2*pi)
-        self._theta2BStep = np.mod(self._points[0]-2*self._dtheta,2*pi)
-        self._theta3BStep = np.mod(self._points[0]-3*self._dtheta,2*pi)
     
     def step( self, f, dt, c, rGIdx = 0 ):
         assert(f.shape==self._nPoints)
-        self._interpolator.compute_interpolant(f,self._spline)
+        
+        zDist = -c*self._bz[rGIdx]*dt
+        
+        # produces 6 unique points unless zDist is an integer
+        # TODO: Fix
+        bkwdShifts = floor( zDist ) - np.array([2,1,0])
+        fwdShifts  = ceil(  zDist ) + np.array([0,1,2])
+        Shifts = np.concatenate([bkwdShifts, fwdShifts])
+        thetaShifts = self._dtheta[rGIdx]*Shifts
+        
+        LagrangeVals = np.ndarray([self._nPoints[1],self._nPoints[0], 6])
+        for i in range(self._nPoints[1]):
+            for j in range(self._nPoints[0]):
+                for k in range(6):
+                    LagrangeVals[i,j,k]=k+j*6+i*6*self._nPoints[0]
+        
+        for i,spline in enumerate(self._thetaSplines):
+            self._interpolator.compute_interpolant(f[:,i],spline)
+            for j,s in enumerate(Shifts):
+                LagrangeVals[(i-s)%self._nPoints[1],:,j]=spline.eval(self._points[0]+thetaShifts[j])
         
         for i,z in enumerate(self._points[1]):
-            zEvalPts = np.take(self._points[1],[i-3,i-2,i-1,i,i+1,i+2,i+3],mode='wrap')
-            zPts = [z-3*self._dz, z-2*self._dz, z-self._dz, z, z+self._dz, z+2*self._dz, z+3*self._dz]
-            for j,theta in enumerate(self._points[0]):
-                thetaPts = [self._theta3BStep[rGIdx][j],self._theta2BStep[rGIdx][j],
-                            self._thetaBStep[rGIdx][j],self._points[0][j],
-                            self._thetaStep[rGIdx][j],self._theta2Step[rGIdx][j],
-                            self._theta3Step[rGIdx][j]]
-                #lagrangePts = self._spline.eval(thetaPts,zEvalPts)
-                #lagrangePts = self._spline.eval(thetaPts,zEvalPts[0])
-                #lagrangePts = self._spline.eval(thetaPts[0],zEvalPts)
-                lagrangePts = [self._spline.eval(thetaPts[0],zEvalPts[0]),
-                               self._spline.eval(thetaPts[1],zEvalPts[1]),
-                               self._spline.eval(thetaPts[2],zEvalPts[2]),
-                               self._spline.eval(thetaPts[3],zEvalPts[3]),
-                               self._spline.eval(thetaPts[4],zEvalPts[4]),
-                               self._spline.eval(thetaPts[5],zEvalPts[5]),
-                               self._spline.eval(thetaPts[6],zEvalPts[6])]
-                poly = lagrange(zPts,lagrangePts)
-                if (self._points[1][i]-c*self._bz[rGIdx]*dt>zPts[-1] or self._points[1][i]-c*self._bz[rGIdx]*dt<zPts[0]):
-                    print("outside")
-                f[j,i] = poly(self._points[1][i]-c*self._bz[rGIdx]*dt)
+            zPts = z+self._dz*Shifts
+            for j in range(self._nPoints[0]):
+                poly = lagrange(zPts,LagrangeVals[i,j,:])
+                f[j,i] = poly(z+zDist)
 
 class vParallelAdvection:
     def __init__( self, eta_vals, splines ):
