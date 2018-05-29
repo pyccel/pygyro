e6dd1da75b787035434a70894a610878ad978cd2
ebourne <louise.bourne@gmail.com>
2018-05-29 18:07:59 +0200

    Wrote visual tests (parallel gradient of phi still needs to be handled)

--- pygyro/advection/visual_test_advection.py
+++ pygyro/advection/visual_test_advection.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot    as plt
 from mpl_toolkits.mplot3d import Axes3D
 from math                 import pi
 
-#import cProfile, pstats, io
-
 from ..                         import splines as spl
 from ..initialisation.setups    import setupCylindricalGrid
 from .advection                 import *
@@ -18,6 +16,7 @@ def test_fluxSurfaceAdvection():
     N = 100
     
     dt=0.1
+    
     c=2
     
     f_vals = np.ndarray([npts[0],npts[1],N+1])
@@ -37,21 +36,9 @@ def test_fluxSurfaceAdvection():
     #f_vals[:,:,0]=np.exp(-((np.atleast_2d(eta_vals[1]).T-pi)**2+(eta_vals[2]-10)**2)/4)
     f_vals[:,:,0]=np.sin(eta_vals[2]*pi/10)
     
-    # profiling:
-    #pr = cProfile.Profile()
-    #pr.enable()
-    
     for n in range(N):
         f_vals[:,:,n+1]=f_vals[:,:,n]
         fluxAdv.step(f_vals[:,:,n+1],dt,c)
-    
-    #pr.disable()
-    #s = io.StringIO()
-    #sortby = 'cumulative'
-    #ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    #ps.print_stats()
-    #print(s.getvalue())
-    
     
     x,y = np.meshgrid(eta_vals[2], eta_vals[1])
     
@@ -79,68 +66,210 @@ def test_fluxSurfaceAdvection():
     print(np.max(f_vals[:,:,n]-f_vals[:,:,0]))
 
 @pytest.mark.serial
-def test_vParallelAdvection():
-    pass
-
-@pytest.mark.serial
-def test_poloidalAdvection():
-    pass
-
-"""
-@pytest.mark.serial
-def test_fluxSurfaceAdvection_gridIntegration():
-    npts = [10,20,10,10]
-    grid = setupCylindricalGrid(npts   = npts,
-                                layout = 'flux_surface')
+def test_poloidalAdvection_invariantPhi():
+    npts = [30,20]
+    eta_vals = [np.linspace(0,20,npts[1],endpoint=False),np.linspace(0,2*pi,npts[0],endpoint=False),
+                np.linspace(0,1,4),np.linspace(0,1,4)]
     
+    N = 200
     dt=0.1
     
-    fluxAdv = fluxSurfaceAdvection(grid.eta_grid, grid.get2DSpline())
+    v=0
     
-    for i,r in grid.getCoords(0):
-        for j,v in grid.getCoords(1):
-            fluxAdv.step(grid.get2DSlice([i,j]),dt,v)
-"""
-
-@pytest.mark.serial
-def test_fluxSurfaceAdvection_gridIntegration():
-    npts = [4,20,20,4]
-    grid = setupCylindricalGrid(npts   = npts,
-                                layout = 'flux_surface')
+    f_vals = np.ndarray([npts[1],npts[0],N+1])
     
-    #N = 20
-    N = 2
+    deg = 3
     
-    dt=0.1
-    c=1
+    domain    = [ [0.1,14.5], [0,2*pi] ]
+    periodic  = [ False, True ]
+    nkts      = [n+1+deg*(int(p)-1)            for (n,p)      in zip( npts, periodic )]
+    breaks    = [np.linspace( *lims, num=num ) for (lims,num) in zip( domain, nkts )]
+    knots     = [spl.make_knots( b,deg,p )     for b,p        in zip(breaks,periodic)]
+    bsplines  = [spl.BSplines( k,deg,p )       for k,p        in zip(knots,periodic)]
+    eta_grids = [bspl.greville                 for bspl       in bsplines]
     
-    f_vals = np.ndarray([npts[1],npts[2],N])
+    eta_vals[0]=eta_grids[0]
+    eta_vals[1]=eta_grids[1]
     
-    fluxAdv = fluxSurfaceAdvection(grid.eta_grid, grid.get2DSpline())
+    polAdv = poloidalAdvection(eta_vals, bsplines[::-1])
     
-    f_vals[:,:,0]=grid.get2DSlice([0,0])
-    for n in range(1,N):
-        fluxAdv.step(grid.get2DSlice([0,0]),dt,c)
-        f_vals[:,:,n]=grid.get2DSlice([0,0])
+    phi = Spline2D(bsplines[1],bsplines[0])
+    phiVals = np.empty([npts[1],npts[0]])
+    phiVals[:]=3*eta_vals[0]**2 * (1+ 1e-1 * np.cos(np.atleast_2d(eta_vals[1]).T*2))
+    #phiVals[:]=10*eta_vals[0]
+    interp = SplineInterpolator2D(bsplines[1],bsplines[0])
     
-    x,y = np.meshgrid(grid.eta_grid[1], grid.eta_grid[2])
+    interp.compute_interpolant(phiVals,phi)
+    
+    #f_vals[:,:,0] = np.exp(-np.atleast_2d((eta_vals[1]-pi)**2).T - (eta_vals[0]-7)**2)/4 + fEq(0.1,v)
+    f_vals[:,:,0] = phiVals + fEq(0.1,v)
+    
+    for n in range(N):
+        f_vals[:,:,n+1]=f_vals[:,:,n]
+        polAdv.step(f_vals[:,:,n+1],dt,phi,v)
+    
+    f_min = np.min(f_vals)
+    f_max = np.max(f_vals)
     
     plt.ion()
 
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    line1 = ax.pcolormesh(x,y,f_vals[:,:,0])
-    plt.show()
-    """
+    ax = plt.subplot(111, projection='polar')
+    #ax = fig.add_axes([0.1, 0.25, 0.7, 0.7],)
+    colorbarax2 = fig.add_axes([0.85, 0.1, 0.03, 0.8],)
+    
+    plotParams = {'vmin':f_min,'vmax':f_max, 'cmap':"jet"}
+    
+    line1 = ax.contourf(eta_vals[1],eta_vals[0],f_vals[:,:,0].T,20,**plotParams)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    
+    fig.colorbar(line1, cax = colorbarax2)
+    
     for n in range(1,N):
+        for coll in line1.collections:
+            coll.remove()
         del line1
-        line1 = ax.plot_surface(x,y, f_vals[:,:,n],color = 'b')
+        line1 = ax.contourf(eta_vals[1],eta_vals[0],f_vals[:,:,n].T,20,**plotParams)
         fig.canvas.draw()
         fig.canvas.flush_events()
-    """
 
 @pytest.mark.serial
-def test_vParallelAdvection_gridIntegration():
+def test_poloidalAdvection_vortex():
+    npts = [30,20]
+    eta_vals = [np.linspace(0,20,npts[1],endpoint=False),np.linspace(0,2*pi,npts[0],endpoint=False),
+                np.linspace(0,1,4),np.linspace(0,1,4)]
+    
+    N = 200
+    dt=0.1
+    
+    v=0
+    
+    f_vals = np.ndarray([npts[1],npts[0],N+1])
+    
+    deg = 3
+    
+    domain    = [ [0.1,14.5], [0,2*pi] ]
+    periodic  = [ False, True ]
+    nkts      = [n+1+deg*(int(p)-1)            for (n,p)      in zip( npts, periodic )]
+    breaks    = [np.linspace( *lims, num=num ) for (lims,num) in zip( domain, nkts )]
+    knots     = [spl.make_knots( b,deg,p )     for b,p        in zip(breaks,periodic)]
+    bsplines  = [spl.BSplines( k,deg,p )       for k,p        in zip(knots,periodic)]
+    eta_grids = [bspl.greville                 for bspl       in bsplines]
+    
+    eta_vals[0]=eta_grids[0]
+    eta_vals[1]=eta_grids[1]
+    
+    polAdv = poloidalAdvection(eta_vals, bsplines[::-1])
+    
+    phi = Spline2D(bsplines[1],bsplines[0])
+    phiVals = np.empty([npts[1],npts[0]])
+    phiVals[:]=10*eta_vals[0]
+    interp = SplineInterpolator2D(bsplines[1],bsplines[0])
+    
+    interp.compute_interpolant(phiVals,phi)
+    
+    f_vals[:,:,0] = np.exp(-np.atleast_2d((eta_vals[1]-pi)**2).T - (eta_vals[0]-7)**2)/4 + fEq(0.1,v)
+    
+    for n in range(N):
+        f_vals[:,:,n+1]=f_vals[:,:,n]
+        polAdv.step(f_vals[:,:,n+1],dt,phi,v)
+    
+    f_min = np.min(f_vals)
+    f_max = np.max(f_vals)
+    
+    plt.ion()
+
+    fig = plt.figure()
+    ax = plt.subplot(111, projection='polar')
+    #ax = fig.add_axes([0.1, 0.25, 0.7, 0.7],)
+    colorbarax2 = fig.add_axes([0.85, 0.1, 0.03, 0.8],)
+    
+    plotParams = {'vmin':f_min,'vmax':f_max, 'cmap':"jet"}
+    
+    line1 = ax.contourf(eta_vals[1],eta_vals[0],f_vals[:,:,0].T,20,**plotParams)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    
+    fig.colorbar(line1, cax = colorbarax2)
+    
+    for n in range(1,N):
+        for coll in line1.collections:
+            coll.remove()
+        del line1
+        line1 = ax.contourf(eta_vals[1],eta_vals[0],f_vals[:,:,n].T,20,**plotParams)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+@pytest.mark.serial
+def test_poloidalAdvection_constantAdv():
+    npts = [30,20]
+    eta_vals = [np.linspace(0,20,npts[1],endpoint=False),np.linspace(0,2*pi,npts[0],endpoint=False),
+                np.linspace(0,1,4),np.linspace(0,1,4)]
+    
+    N = 200
+    dt=0.1
+    
+    v=0
+    
+    f_vals = np.ndarray([npts[1],npts[0],N+1])
+    
+    deg = 3
+    
+    domain    = [ [0.1,14.5], [0,2*pi] ]
+    periodic  = [ False, True ]
+    nkts      = [n+1+deg*(int(p)-1)            for (n,p)      in zip( npts, periodic )]
+    breaks    = [np.linspace( *lims, num=num ) for (lims,num) in zip( domain, nkts )]
+    knots     = [spl.make_knots( b,deg,p )     for b,p        in zip(breaks,periodic)]
+    bsplines  = [spl.BSplines( k,deg,p )       for k,p        in zip(knots,periodic)]
+    eta_grids = [bspl.greville                 for bspl       in bsplines]
+    
+    eta_vals[0]=eta_grids[0]
+    eta_vals[1]=eta_grids[1]
+    
+    polAdv = poloidalAdvection(eta_vals, bsplines[::-1])
+    
+    phi = Spline2D(bsplines[1],bsplines[0])
+    phiVals = np.empty([npts[1],npts[0]])
+    phiVals[:]=3*eta_vals[0]**2
+    interp = SplineInterpolator2D(bsplines[1],bsplines[0])
+    
+    interp.compute_interpolant(phiVals,phi)
+    
+    f_vals[:,:,0] = np.exp(-np.atleast_2d((eta_vals[1]-pi)**2).T - (eta_vals[0]-7)**2)/4 + fEq(0.1,v)
+    
+    for n in range(N):
+        f_vals[:,:,n+1]=f_vals[:,:,n]
+        polAdv.step(f_vals[:,:,n+1],dt,phi,v)
+    
+    f_min = np.min(f_vals)
+    f_max = np.max(f_vals)
+    
+    plt.ion()
+
+    fig = plt.figure()
+    ax = plt.subplot(111, projection='polar')
+    #ax = fig.add_axes([0.1, 0.25, 0.7, 0.7],)
+    colorbarax2 = fig.add_axes([0.85, 0.1, 0.03, 0.8],)
+    
+    plotParams = {'vmin':f_min,'vmax':f_max, 'cmap':"jet"}
+    
+    line1 = ax.contourf(eta_vals[1],eta_vals[0],f_vals[:,:,0].T,20,**plotParams)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    
+    fig.colorbar(line1, cax = colorbarax2)
+    
+    for n in range(1,N):
+        for coll in line1.collections:
+            coll.remove()
+        del line1
+        line1 = ax.contourf(eta_vals[1],eta_vals[0],f_vals[:,:,n].T,20,**plotParams)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+@pytest.mark.serial
+def test_vParallelAdvection():
     npts = [4,4,4,100]
     grid = setupCylindricalGrid(npts   = npts,
                                 layout = 'v_parallel')
@@ -148,7 +277,8 @@ def test_vParallelAdvection_gridIntegration():
     N = 100
     
     dt=0.1
-    c=1
+    
+    c = 1.0
     
     f_vals = np.ndarray([npts[3],N])
     
@@ -170,14 +300,56 @@ def test_vParallelAdvection_gridIntegration():
         fig.canvas.draw()
         fig.canvas.flush_events()
 
-@pytest.mark.serial
-def test_poloidalAdvection_gridIntegration():
-    npts = [10,20,10,10]
+@pytest.mark.parallel
+def test_equilibrium():
+    npts = [4,4,4,4]
     grid = setupCylindricalGrid(npts   = npts,
-                                layout = 'poloidal')
+                                layout = 'flux_surface',
+                                eps    = 0)
     
-    polAdv = poloidalAdvection(grid.eta_grid, grid.get2DSpline())
+    fluxAdv = fluxSurfaceAdvection(grid.eta_grid, grid.get2DSpline())
+    vParAdv = vParallelAdvection(grid.eta_grid, grid.getSpline(3))
+    polAdv = poloidalAdvection(grid.eta_grid, grid.getSpline(slice(1,None,-1)))
     
-    #~ for i,z in grid.getCoords(0):
-        #~ for j,v in grid.getCoords(1):
-            #~ poloidalAdv(grid.get2DSlice([i,j]))
+    N=10
+    dt=0.1
+    halfStep = dt*0.5
+    
+    phi = Spline2D(grid.getSpline(1),grid.getSpline(0))
+    phiVals = np.empty([npts[1],npts[0]])
+    phiVals[:]=3
+    #phiVals[:]=10*eta_vals[0]
+    interp = SplineInterpolator2D(grid.getSpline(1),grid.getSpline(0))
+    
+    
+    for n in range(N):
+        for i,r in grid.getCoords(0):
+            for j,v in grid.getCoords(1):
+                fluxAdv.step(grid.get2DSlice([i,j]),halfStep,v)
+        
+        grid.setLayout('v_parallel')
+        
+        for i,r in grid.getCoords(0):
+            for j,z in grid.getCoords(1):
+                for k,q in grid.getCoords(2):
+                    vParAdv.step(grid.get1DSlice([i,j,k]),halfStep,0,r)
+        
+        grid.setLayout('poloidal')
+        
+        for i,v in grid.getCoords(0):
+            for j,z in grid.getCoords(1):
+                polAdv.step(grid.get2DSlice([i,j]),dt,phi,v)
+        
+        grid.setLayout('v_parallel')
+        
+        for i,r in grid.getCoords(0):
+            for j,z in grid.getCoords(1):
+                for k,q in grid.getCoords(2):
+                    vParAdv.step(grid.get1DSlice([i,j,k]),halfStep,0,r)
+        
+        grid.setLayout('flux_surface')
+        
+        for i,r in grid.getCoords(0):
+            for j,v in grid.getCoords(1):
+                fluxAdv.step(grid.get2DSlice([i,j]),halfStep,v)
+    
