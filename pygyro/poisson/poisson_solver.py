Corrected sign error due to integration by parts

pygyro/poisson/poisson_solver.py
@@ -375,7 +375,7 @@ class PoissonSolver:
         
         # Collect the factors in front of each element of the equation
         r = eta_grid[0][self._coeff_range]
-        ddrFactor = kwargs.pop('ddrFactor',-1)
+        ddrFactor = kwargs.pop('ddrFactor',1)
         drFactor = kwargs.pop('drFactor',-( 1/r - constants.kN0 * \
                                 (1 + np.tanh( (r - constants.rp ) / \
                                               constants.deltaRN0 )**2 ) ))