a8a4aebe7b92d4a45d59e342d3597e0a26a20b72
ebourne <louise.bourne@gmail.com>
2018-07-11 13:58:09 +0200

    Renamed left and right Boundary to lower and upper Boundary. Added test using all parts of the quasi-neutrality equation in their original form

--- pygyro/poisson/test_poisson_solver.py
+++ pygyro/poisson/test_poisson_solver.py
@@ -3,12 +3,14 @@ import numpy                as np
 import pytest
 from math                   import pi
 
-from ..model.process_grid       import compute_2d_process_grid
-from ..model.layout             import LayoutSwapper, getLayoutHandler
-from ..model.grid               import Grid
-from ..initialisation.setups    import setupCylindricalGrid
-from ..                         import splines as spl
-from .poisson_solver            import PoissonSolver, DensityFinder
+from ..model.process_grid           import compute_2d_process_grid
+from ..model.layout                 import LayoutSwapper, getLayoutHandler
+from ..model.grid                   import Grid
+from ..initialisation.setups        import setupCylindricalGrid
+from ..initialisation.initialiser   import Te
+from ..initialisation               import constants
+from ..                             import splines as spl
+from .poisson_solver                import PoissonSolver, DensityFinder
 from ..splines.splines              import BSplines, Spline1D
 from ..splines.spline_interpolators import SplineInterpolator1D
 
@@ -112,7 +114,7 @@ def test_BasicPoissonEquation_rNeumann(deg,npt,eps):
     layout_poisson = {'mode_solve': [1,2,0]}
     remapper = getLayoutHandler(comm,layout_poisson,[comm.Get_size()],eta_grid)
     
-    ps = PoissonSolver(eta_grid,2*deg,bsplines[0],rBoundary='neumann',drFactor=0,rFactor=0,ddThetaFactor=0)
+    ps = PoissonSolver(eta_grid,2*deg,bsplines[0],uBoundary='neumann',drFactor=0,rFactor=0,ddThetaFactor=0)
     phi=Grid(eta_grid,bsplines,remapper,'mode_solve',comm,dtype=np.complex128)
     phi_exact=Grid(eta_grid,bsplines,remapper,'mode_solve',comm,dtype=np.complex128)
     rho=Grid(eta_grid,bsplines,remapper,'mode_solve',comm,dtype=np.complex128)
@@ -396,7 +398,7 @@ def test_ddTheta(deg,npt):
     remapper = getLayoutHandler(comm,layout_poisson,[comm.Get_size()],eta_grid)
     
     ps = PoissonSolver(eta_grid,2*deg,bsplines[0],ddrFactor=0,drFactor=0,
-                        rFactor=1,ddThetaFactor=-1,lBoundary='neumann',rBoundary='neumann')
+                        rFactor=1,ddThetaFactor=-1,lBoundary='neumann',uBoundary='neumann')
     phi=Grid(eta_grid,bsplines,remapper,'mode_solve',comm,dtype=np.complex128)
     phi_exact=Grid(eta_grid,bsplines,remapper,'v_parallel',comm,dtype=np.complex128)
     rho=Grid(eta_grid,bsplines,remapper,'v_parallel',comm,dtype=np.complex128)
@@ -450,7 +452,7 @@ def test_phi(deg,npt):
     a=2*pi/(domain[0][1]-domain[0][0])
     
     ps = PoissonSolver(eta_grid,2*deg,bsplines[0],ddrFactor=0,drFactor=0,rFactor=1,ddThetaFactor=0,
-                        lBoundary='neumann',rBoundary='neumann')
+                        lBoundary='neumann',uBoundary='neumann')
     phi=Grid(eta_grid,bsplines,remapper,'mode_solve',comm,dtype=np.complex128)
     phi_exact=Grid(eta_grid,bsplines,remapper,'v_parallel',comm,dtype=np.complex128)
     rho=Grid(eta_grid,bsplines,remapper,'v_parallel',comm,dtype=np.complex128)
@@ -476,6 +478,62 @@ def test_phi(deg,npt):
     
     #~ print(np.max(np.abs(phi._f-phi_exact._f)))
     assert((np.abs(phi._f-phi_exact._f)<eps).all())
+
+@pytest.mark.serial
+def test_quasiNeutrality():
+    npts = [256,32,4]
+    domain = [[constants.rMin,constants.rMax],[0,2*pi],[0,1]]
+    degree = [3,3,3]
+    period = [False,True,False]
+    comm = MPI.COMM_WORLD
+    
+    # Compute breakpoints, knots, spline space and grid points
+    nkts     = [n+1+d*(int(p)-1)              for (n,d,p)    in zip( npts,degree, period )]
+    breaks   = [np.linspace( *lims, num=num ) for (lims,num) in zip( domain, nkts )]
+    knots    = [spl.make_knots( b,d,p )       for (b,d,p)    in zip( breaks, degree, period )]
+    bsplines = [spl.BSplines( k,d,p )         for (k,d,p)    in zip(  knots, degree, period )]
+    eta_grid = [bspl.greville                 for bspl       in bsplines]
+    
+    layout_poisson = {'mode_solve': [1,2,0],
+                      'v_parallel': [0,2,1]}
+    remapper = getLayoutHandler(comm,layout_poisson,[comm.Get_size()],eta_grid)
+    
+    a=1.5*pi/(domain[0][1]-domain[0][0])
+    
+    #~ ps = PoissonSolver(eta_grid,6,bsplines[0],lBoundary='neumann')
+    ps = PoissonSolver(eta_grid,6,bsplines[0],lBoundary='neumann')
+    phi=Grid(eta_grid,bsplines,remapper,'mode_solve',comm,dtype=np.complex128)
+    phi_exact=Grid(eta_grid,bsplines,remapper,'v_parallel',comm,dtype=np.complex128)
+    rho=Grid(eta_grid,bsplines,remapper,'v_parallel',comm,dtype=np.complex128)
+    
+    q = eta_grid[1]
+    
+    for i,r in rho.getCoords(0):
+        rArg = a*(r-domain[0][0])
+        plane = rho.get2DSlice([i])
+        plane[:] = -12*np.cos(rArg)**2*np.sin(rArg)**2*a*a*np.sin(q)**3 \
+                   + 4*np.cos(rArg)**4                *a*a*np.sin(q)**3 \
+                   + (1/r - constants.kN0*(1-np.tanh((r-constants.rp)/constants.deltaRN0)**2)) * \
+                   4 * np.cos(rArg)**3*np.sin(rArg)*a*np.sin(q)**3 \
+                   + np.cos(rArg)**4*np.sin(q)**3 / Te(r) \
+                   - 6 * np.cos(rArg)**4*np.sin(q)*np.cos(q)**2/r**2 \
+                   + 3 * np.cos(rArg)**4*np.sin(q)**3/r**2
+        plane = phi_exact.get2DSlice([i])
+        plane[:] = np.cos(rArg)**4*np.sin(q)**3
+    
+    r = eta_grid[0]
+    
+    ps.getModes(rho)
+    
+    rho.setLayout('mode_solve')
+    
+    ps.solveEquation(phi,rho)
+    
+    phi.setLayout('v_parallel')
+    ps.findPotential(phi)
+    
+    #~ print(np.max(np.abs(phi._f-phi_exact._f)))
+    assert((np.abs(phi._f-phi_exact._f)<0.1).all())
 
 @pytest.mark.parallel
 def test_PoissonSolver():
