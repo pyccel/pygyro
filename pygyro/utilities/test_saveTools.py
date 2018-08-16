bddc791d5d7e31a253fcfdc2275d36106ced2100
ebourne <louise.bourne@gmail.com>
2018-08-16 12:55:31 +0200

    Deleting is not necessary

--- pygyro/utilities/test_saveTools.py
+++ pygyro/utilities/test_saveTools.py
@@ -1,3 +1,4 @@
+from mpi4py import MPI
 from .savingTools           import setupSave
 import os
 import pytest
@@ -24,12 +25,3 @@ def test_Save_p():
     n1=setupSave(3,3,3,3,npts,0.1)
     n2=setupSave(3,3,3,3,npts,0.1)
     n3=setupSave(3,3,3,3,npts,0.1,"testFilename")
-    assert(os.path.isdir(n1))
-    assert(os.path.isdir(n2))
-    assert(os.path.isdir(n3))
-    os.remove("{0}/initParams.h5".format(n1))
-    os.remove("{0}/initParams.h5".format(n2))
-    os.remove("{0}/initParams.h5".format(n3))
-    os.rmdir(n1)
-    os.rmdir(n2)
-    os.rmdir(n3)
