7aef3f66935e33e60ff24de0d3058a0e9334109d
ebourne <louise.bourne@gmail.com>
2018-08-16 11:31:43 +0200

    Added tests for initialisation from precalculated data

--- pygyro/initialisation/test_setup.py
+++ pygyro/initialisation/test_setup.py
@@ -2,10 +2,8 @@ from mpi4py import MPI
 import numpy as np
 from functools import reduce
 import pytest
-import h5py
 
 from  .setups                 import setupCylindricalGrid, setupFromFile
-from  .printInitialVals       import getParameters
 
 @pytest.mark.serial
 def test_setup():
@@ -49,14 +47,25 @@ def test_setup():
     for (coord,npt) in zip(grid.eta_grid,npts):
         assert(len(coord)==npt)
 
+def compare_f(grid):
+    [nEta1,nEta2,nEta3,nEta4] = grid.nGlobalCoords
+    
+    for i,x in grid.getCoords(0):
+        for j,y in grid.getCoords(1):
+            for k,z in grid.getCoords(2):
+                Slice = grid.get1DSlice([i,j,k])
+                for l,a in enumerate(Slice):
+                    [I,J,K,L] = grid.getGlobalIndices([i,j,k,l])
+                    assert(a == I*nEta4*nEta3*nEta2+J*nEta4*nEta3+K*nEta4+L)
+
 @pytest.mark.parallel
-def test_printVals():
-    npts = [10,20,10,10]
+def test_setupFromFolder():
     
-    test_file = h5py.File('test_init.h5','w',driver='mpio',comm=MPI.COMM_WORLD)
-    getParameters(test_file,3,3,3,3,npts)
-    test_file.close()
+    grid = setupFromFile('__test__')
+    compare_f(grid)
+
+@pytest.mark.parallel
+def test_setupFromFolderAtTime():
     
-    test_file = h5py.File('test_init.h5','r',driver='mpio',comm=MPI.COMM_WORLD)
-    grid = setupFromFile(test_file,layout = 'v_parallel')
-    test_file.close()
+    grid = setupFromFile('__test__',timepoint = 40)
+    compare_f(grid)
