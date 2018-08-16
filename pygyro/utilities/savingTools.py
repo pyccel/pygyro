321464b25325d82067f5f1799a5a88a10dd401a5
ebourne <louise.bourne@gmail.com>
2018-08-16 12:53:01 +0200

    Added comm argument. Only one rank creates folder.

--- pygyro/utilities/savingTools.py
+++ pygyro/utilities/savingTools.py
@@ -3,22 +3,29 @@ import os
 import inspect
 import numpy        as np
 import h5py
+from glob import glob
 
 from ..initialisation          import constants
 
-def setupSave(rDegree,qDegree,zDegree,vDegree,npts,dt,foldername: str = None):
-    if (foldername==None):
-        i=0
-        foldername="simulation_{0}".format(i)
-        while(os.path.isdir(foldername)):
-            i+=1
+def setupSave(rDegree,qDegree,zDegree,vDegree,npts,dt,foldername: str = None,
+                comm: MPI.Comm = MPI.COMM_WORLD, root: int = 0,test: bool = False):
+    if (comm.Get_rank()==root):
+        if (foldername==None):
+            i=0
             foldername="simulation_{0}".format(i)
-        os.mkdir(foldername)
-    else:
-        if (not os.path.isdir(foldername)):
+            while(os.path.isdir(foldername)):
+                i+=1
+                foldername="simulation_{0}".format(i)
             os.mkdir(foldername)
-    filename = "{0}/initParams.h5".format(foldername)
-    saveFile = h5py.File(filename,'w',driver='mpio',comm=MPI.COMM_WORLD)
+            foldername=comm.bcast(foldername,root=root)
+        else:
+            if (not os.path.isdir(foldername)):
+                os.mkdir(foldername)
+    else:
+        if (foldername==None):
+            foldername=comm.bcast("",root=root)
+    filename = '{0}/initParams.h5'.format(foldername)
+    saveFile = h5py.File(filename,'w',driver='mpio',comm=comm)
     printParameters(saveFile,rDegree,qDegree,zDegree,vDegree,npts,dt)
     saveFile.close()
     return foldername
