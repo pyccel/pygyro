17f738ba4e499f3e1e597ed316027fa23b1964d7
ebourne <louise.bourne@gmail.com>
2018-08-16 19:40:50 +0200

    Changed to correct simulation parameters

--- pygyro/initialisation/constants.py
+++ pygyro/initialisation/constants.py
@@ -1,14 +1,14 @@
 import numpy as np
 from scipy import integrate
-from math import exp, tanh
+from math import exp, tanh, pi
 
 B0 = 1.0
 R0 = 239.8081535
 rMin = 0.1
 rMax = 14.5
 zMin = 0.0
-zMax = 80.0
-vMax = 5.0
+zMax = R0*2*pi
+vMax = 7.32
 vMin = -vMax
 rp = 0.5*(rMin + rMax)
 eps = 1e-6
@@ -23,7 +23,7 @@ deltaR = 4.0*deltaRN0/deltaRTi
 CTi = 1.0
 CTe = float(CTi)
 m = 15
-n = -11
+n = 1
 iotaVal = 0.0
 
 def iota(r = rp):
