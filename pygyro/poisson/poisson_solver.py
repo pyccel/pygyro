ad02605a04d56e68fe36b9e53c37848b6ed31f33
ebourne <louise.bourne@gmail.com>
2018-08-20 13:28:52 +0200

    Corrected ill defined condition

--- pygyro/poisson/poisson_solver.py
+++ pygyro/poisson/poisson_solver.py
@@ -156,9 +156,9 @@ class DiffEqSolver:
         
         # Ensure dirichlet boundaries are not used at both boundaries on
         # any mode
-        #~ poorlyDefined = [b for b in lNeumannIdx if b in uNeumannIdx]
-        #~ if (len(poorlyDefined)!=0):
-            #~ raise ValueError("Modes {0} are poorly defined as they use 0 Dirichlet boundary conditions".format(poorlyDefined))
+        poorlyDefined = [b for b in lNeumannIdx if b in uNeumannIdx]
+        if (rFactor==(lambda r:0) and len(poorlyDefined)!=0):
+            raise ValueError("Modes {0} are poorly defined as they use 0 Dirichlet boundary conditions".format(poorlyDefined))
         
         # If dirichlet boundary conditions are used then assign the
         # required value on the boundary
