9c57748596543232c1e139b72953de5fd2bb3ce6
ebourne <louise.bourne@gmail.com>
2018-09-12 15:48:56 +0200

    Fixed errors

--- pygyro/splines/spline_eval_funcs.py
+++ pygyro/splines/spline_eval_funcs.py
@@ -40,7 +40,7 @@ def eval_spline_1d_vector(x,knots,degree,coeffs,y,der=0):
                 y[i]+=coeffs[span-degree+j]*basis[j]
     return y
 
-@types('double','double[:]','double[:]','int','double[:]','int','double[:,:]','int','int')
+@types('double','double','double[:]','int','double[:]','int','double[:,:]','int','int')
 def eval_spline_2d_scalar(x,y,kts1,deg1,kts2,deg2,coeffs,der1=0,der2=0):
     span1  =  find_span( kts1, deg1, x )
     span2  =  find_span( kts2, deg2, y )
@@ -58,7 +58,7 @@ def eval_spline_2d_scalar(x,y,kts1,deg1,kts2,deg2,coeffs,der1=0,der2=0):
         basis_funs_1st_der( kts2, deg2, y, span2, basis2 )
     
     theCoeffs = empty([deg1+1,deg2+1])
-    theCoeffs[:] = coeffs[span1-deg1:span1+1,span2-deg2:span2+1]
+    theCoeffs[:,:] = coeffs[span1-deg1:span1+1,span2-deg2:span2+1]
     
     z = 0.0
     for i in range(deg1+1):
@@ -72,6 +72,7 @@ def eval_spline_2d_scalar(x,y,kts1,deg1,kts2,deg2,coeffs,der1=0,der2=0):
 @types('double[:]','double[:]','double[:]','int','double[:]','int','double[:,:]','double[:]','int','int')
 def eval_spline_2d_vector(x,y,kts1,deg1,kts2,deg2,coeffs,z,der1=0,der2=0):
     from numpy      import empty
+    theCoeffs = empty([deg1+1,deg2+1])
     if (der1==0):
         if (der2==0):
             for i in range(len(x)):
@@ -82,7 +83,7 @@ def eval_spline_2d_vector(x,y,kts1,deg1,kts2,deg2,coeffs,z,der1=0,der2=0):
                 basis_funs( kts1, deg1, x[i], span1, basis1 )
                 basis_funs( kts2, deg2, y[i], span2, basis2 )
                 
-                theCoeffs = coeffs[span1-deg1:span1+1,span2-deg2:span2+1].copy()
+                theCoeffs[:,:] = coeffs[span1-deg1:span1+1,span2-deg2:span2+1]
                 
                 z[i] = 0.0
                 for j in range(deg1+1):
@@ -99,7 +100,7 @@ def eval_spline_2d_vector(x,y,kts1,deg1,kts2,deg2,coeffs,z,der1=0,der2=0):
                 basis_funs( kts1, deg1, x[i], span1, basis1 )
                 basis_funs_1st_der( kts2, deg2, y[i], span2, basis2 )
                 
-                theCoeffs = coeffs[span1-deg1:span1+1,span2-deg2:span2+1].copy()
+                theCoeffs[:,:] = coeffs[span1-deg1:span1+1,span2-deg2:span2+1]
                 
                 z[i] = 0.0
                 for j in range(deg1+1):
@@ -117,7 +118,7 @@ def eval_spline_2d_vector(x,y,kts1,deg1,kts2,deg2,coeffs,z,der1=0,der2=0):
                 basis_funs_1st_der( kts1, deg1, x[i], span1, basis1 )
                 basis_funs( kts2, deg2, y[i], span2, basis2 )
                 
-                theCoeffs = coeffs[span1-deg1:span1+1,span2-deg2:span2+1].copy()
+                theCoeffs[:,:] = coeffs[span1-deg1:span1+1,span2-deg2:span2+1]
                 
                 z[i] = 0.0
                 for j in range(deg1+1):
@@ -134,7 +135,7 @@ def eval_spline_2d_vector(x,y,kts1,deg1,kts2,deg2,coeffs,z,der1=0,der2=0):
                 basis_funs_1st_der( kts1, deg1, x[i], span1, basis1 )
                 basis_funs_1st_der( kts2, deg2, y[i], span2, basis2 )
                 
-                theCoeffs = coeffs[span1-deg1:span1+1,span2-deg2:span2+1].copy()
+                theCoeffs[:,:] = coeffs[span1-deg1:span1+1,span2-deg2:span2+1]
                 
                 z[i] = 0.0
                 for j in range(deg1+1):
@@ -142,4 +143,3 @@ def eval_spline_2d_vector(x,y,kts1,deg1,kts2,deg2,coeffs,z,der1=0,der2=0):
                     for k in range(1,deg2+1):
                         theCoeffs[j,0] += theCoeffs[j,k]*basis2[k]
                     z[i]+=theCoeffs[j,0]*basis1[j]
-    
