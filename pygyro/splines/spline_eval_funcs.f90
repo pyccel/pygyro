155a1dc4837e5dcdf4f0c5333023aafb1af0eed9
ebourne <louise.bourne@gmail.com>
2018-09-18 11:53:01 +0200

    Module name change

--- pygyro/splines/spline_eval_funcs.f90
+++ pygyro/splines/spline_eval_funcs.f90
@@ -1,4 +1,4 @@
-module mod_spline_eval_funcs
+module mod_pygyro_splines_spline_eval_funcs
 
 use mod_context_1, only: find_span
 use mod_context_1, only: basis_funs
@@ -14,6 +14,7 @@ subroutine eval_spline_1d_scalar(x, n0_knots, knots, degree, n0_coeffs, &
       coeffs, der, y)
 
   implicit none
+  real(kind=8), intent(out)  :: y
   real(kind=8), intent(in)  :: x
   integer(kind=4), intent(in)  :: n0_knots
   real(kind=8), intent(in)  :: knots (0:n0_knots - 1)
@@ -21,10 +22,9 @@ subroutine eval_spline_1d_scalar(x, n0_knots, knots, degree, n0_coeffs, &
   integer(kind=4), intent(in)  :: n0_coeffs
   real(kind=8), intent(in)  :: coeffs (0:n0_coeffs - 1)
   integer(kind=4), intent(in)  :: der
-  integer(kind=4) :: j
   real(kind=8), allocatable :: basis (:)
   integer(kind=4) :: span
-  real(kind=8), intent(out)  :: y
+  integer(kind=4) :: j
 
   span = find_span(knots, degree, x)
 
@@ -64,10 +64,10 @@ subroutine eval_spline_1d_vector(n0_x, x, n0_knots, knots, degree, &
   integer(kind=4), intent(in)  :: n0_y
   real(kind=8), intent(inout)  :: y (0:n0_y - 1)
   integer(kind=4), intent(in)  :: der
-  integer(kind=4) :: j
+  real(kind=8), allocatable :: basis (:)
   integer(kind=4) :: span
   integer(kind=4) :: i
-  real(kind=8), allocatable :: basis (:)
+  integer(kind=4) :: j
 
 
   if (der == 0 ) then
@@ -110,6 +110,7 @@ subroutine eval_spline_2d_scalar(x, y, n0_kts1, kts1, deg1, n0_kts2, &
       kts2, deg2, n0_coeffs, n1_coeffs, coeffs, der1, der2, z)
 
   implicit none
+  real(kind=8), intent(out)  :: z
   real(kind=8), intent(in)  :: x
   real(kind=8), intent(in)  :: y
   integer(kind=4), intent(in)  :: n0_kts1
@@ -123,14 +124,13 @@ subroutine eval_spline_2d_scalar(x, y, n0_kts1, kts1, deg1, n0_kts2, &
   real(kind=8), intent(in)  :: coeffs (0:n0_coeffs - 1,0:n1_coeffs - 1)
   integer(kind=4), intent(in)  :: der1
   integer(kind=4), intent(in)  :: der2
-  integer(kind=4) :: j
-  integer(kind=4) :: i
   integer(kind=4) :: span1
-  real(kind=8), allocatable :: theCoeffs (:,:)
-  real(kind=8), allocatable :: basis1 (:)
-  integer(kind=4) :: span2
   real(kind=8), allocatable :: basis2 (:)
-  real(kind=8), intent(out)  :: z
+  real(kind=8), allocatable :: theCoeffs (:,:)
+  integer(kind=4) :: span2
+  integer(kind=4) :: i
+  real(kind=8), allocatable :: basis1 (:)
+  integer(kind=4) :: j
 
   span1 = find_span(kts1, deg1, x)
   span2 = find_span(kts2, deg2, y)
@@ -195,17 +195,17 @@ subroutine eval_spline_2d_cross(n0_xVec, xVec, n0_yVec, yVec, n0_kts1, &
   real(kind=8), intent(inout)  :: z (0:n0_z - 1,0:n1_z - 1)
   integer(kind=4), intent(in)  :: der1
   integer(kind=4), intent(in)  :: der2
+  integer(kind=4) :: span1
+  real(kind=8), allocatable :: theCoeffs (:,:)
+  real(kind=8), allocatable :: basis2 (:)
+  real(kind=8), allocatable :: basis1 (:)
+  integer(kind=4) :: l
+  integer(kind=4) :: j
+  real(kind=8) :: x
+  integer(kind=4) :: i
   integer(kind=4) :: span2
   real(kind=8) :: y
-  integer(kind=4) :: i
-  real(kind=8), allocatable :: basis2 (:)
-  real(kind=8) :: x
-  integer(kind=4) :: j
-  real(kind=8), allocatable :: theCoeffs (:,:)
-  integer(kind=4) :: span1
-  real(kind=8), allocatable :: basis1 (:)
   integer(kind=4) :: k
-  integer(kind=4) :: l
 
 
   allocate(basis1(0:deg1))
@@ -362,14 +362,14 @@ subroutine eval_spline_2d_vector(n0_x, x, n0_y, y, n0_kts1, kts1, deg1, &
   real(kind=8), intent(inout)  :: z (0:n0_z - 1)
   integer(kind=4), intent(in)  :: der1
   integer(kind=4), intent(in)  :: der2
-  integer(kind=4) :: span2
-  real(kind=8), allocatable :: basis2 (:)
-  integer(kind=4) :: j
-  integer(kind=4) :: k
-  real(kind=8), allocatable :: theCoeffs (:,:)
   integer(kind=4) :: span1
-  integer(kind=4) :: i
+  real(kind=8), allocatable :: theCoeffs (:,:)
+  real(kind=8), allocatable :: basis2 (:)
   real(kind=8), allocatable :: basis1 (:)
+  integer(kind=4) :: j
+  integer(kind=4) :: span2
+  integer(kind=4) :: k
+  integer(kind=4) :: i
 
 
   allocate(theCoeffs(0:deg2, 0:deg1))
