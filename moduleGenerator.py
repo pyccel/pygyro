bb05506892af42154bd6039f9419eb9880d2e73d
ebourne <louise.bourne@gmail.com>
2018-09-18 16:00:32 +0200

    Added .so generation steps for advection

--- moduleGenerator.py
+++ moduleGenerator.py
@@ -22,3 +22,17 @@ init_context.insert_function(fEq, ['double','double','double','double','double',
 init_context.insert_function(perturbation, ['double','double','double','int','int','double','double','double'])
 
 initialiser_func = epyccel(pygyro.initialisation.initialiser_func, context=init_context)
+
+
+import pygyro.advection.accelerated_advection_steps
+from pygyro.splines.mod_spline_eval_funcs import eval_spline_2d_cross, eval_spline_2d_scalar
+from pygyro.initialisation.mod_initialiser_funcs import fEq
+
+spline_context = ContextPyccel(name='spline_eval_funcs',context_folder='pygyro.splines',output_folder='pygyro.advection')
+spline_context.insert_function(eval_spline_2d_cross, ['double[:]','double[:]','double[:]','int','double[:]','int','double[:,:]','double[:,:]','int','int'])
+spline_context.insert_function(eval_spline_2d_scalar, ['double','double','double[:]','int','double[:]','int','double[:,:]','int','int'])
+
+init_context = ContextPyccel(name='initialiser_funcs',context_folder='pygyro.initialisation',output_folder='pygyro.advection')
+init_context.insert_function(fEq, ['double','double','double','double','double','double','double','double','double'])
+
+aas_func = epyccel(pygyro.advection.accelerated_advection_steps, context=[spline_context,init_context])
