87032d30df9940e81561b02cc72638a4b533695f
ebourne <louise.bourne@gmail.com>
2018-09-18 15:59:57 +0200

    Create spline module that can be used from other folders

--- buildLibraries.sh
+++ buildLibraries.sh
@@ -2,4 +2,5 @@
 
 pyccel pygyro/initialisation/mod_initialiser_funcs.py --output pygyro/initialisation --fflags ' -O3 -fPIC'
 pyccel pygyro/splines/mod_context_1.py --output pygyro/splines --fflags ' -O3 -fPIC'
+pyccel pygyro/splines/mod_spline_eval_funcs.py --output pygyro/splines --fflags ' -O3 -fPIC'
 python3 moduleGenerator.py
