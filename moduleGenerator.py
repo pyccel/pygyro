70e8120f9d1214f61057c1cb01361e3ac53b0717
ebourne <louise.bourne@gmail.com>
2018-09-20 16:07:40 +0200

    Add additional function for accelerated parallel gradient

--- moduleGenerator.py
+++ moduleGenerator.py
@@ -21,13 +21,14 @@ init_context.insert_function(perturbation, ['double','double','double','int','in
 initialiser_func = epyccel(pygyro.initialisation.initialiser_func, context=init_context)
 
 import pygyro.advection.accelerated_advection_steps
-from pygyro.splines.mod_spline_eval_funcs import eval_spline_2d_cross, eval_spline_2d_scalar, eval_spline_1d_scalar
+from pygyro.splines.mod_spline_eval_funcs import eval_spline_2d_cross, eval_spline_2d_scalar, eval_spline_1d_scalar, eval_spline_1d_vector
 from pygyro.initialisation.mod_initialiser_funcs import fEq
 
 spline_context = ContextPyccel(name='spline_eval_funcs',context_folder='pygyro.splines',output_folder='pygyro.advection')
 spline_context.insert_function(eval_spline_2d_cross, ['double[:]','double[:]','double[:]','int','double[:]','int','double[:,:]','double[:,:]','int','int'])
 spline_context.insert_function(eval_spline_2d_scalar, ['double','double','double[:]','int','double[:]','int','double[:,:]','int','int'])
 spline_context.insert_function(eval_spline_1d_scalar, ['double','double[:]','int','double[:]','int'])
+spline_context.insert_function(eval_spline_1d_vector, ['double','double[:]','int','double[:]','double[:]','int'])
 
 init_context = ContextPyccel(name='initialiser_funcs',context_folder='pygyro.initialisation',output_folder='pygyro.advection')
 init_context.insert_function(fEq, ['double','double','double','double','double','double','double','double','double'])
