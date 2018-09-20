3c5b622717dba55093c1a683a9ea580f1f56a3c0
ebourne <louise.bourne@gmail.com>
2018-09-20 11:20:01 +0200

    Remove duplicated test

--- pygyro/initialisation/test_setup.py
+++ pygyro/initialisation/test_setup.py
@@ -5,27 +5,6 @@ import pytest
 
 from  .setups                 import setupCylindricalGrid, setupFromFile
 
-@pytest.mark.serial
-def test_setup():
-    npts = [10,20,10,10]
-    grid = setupCylindricalGrid(npts   = npts,
-                                layout = 'flux_surface')
-    
-    for (coord,npt) in zip(grid.eta_grid,npts):
-        assert(len(coord)==npt)
-    
-    grid = setupCylindricalGrid(npts   = npts,
-                                layout = 'poloidal')
-    
-    for (coord,npt) in zip(grid.eta_grid,npts):
-        assert(len(coord)==npt)
-    
-    grid = setupCylindricalGrid(npts   = npts,
-                                layout = 'v_parallel')
-    
-    for (coord,npt) in zip(grid.eta_grid,npts):
-        assert(len(coord)==npt)
-
 @pytest.mark.parallel
 def test_setup():
     npts = [10,20,10,10]
