6c60583281af4bdba58a937ef5fff5dc0e25aff1
ebourne <louise.bourne@gmail.com>
2018-10-16 14:38:50 +0200

    Remove comments. Remove prints. Change to right init shape

--- pygyro/advection/convergence_test_advection.py
+++ pygyro/advection/convergence_test_advection.py
@@ -9,7 +9,7 @@ from ..initialisation.mod_initialiser_funcs     import fEq
 from .advection                     import *
 from ..                             import splines as spl
 from ..initialisation               import constants
-"""
+
 def gaussLike(x):
     return np.cos(pi*x*0.1)**4
 
@@ -306,7 +306,7 @@ def test_poloidalAdvection_constantAdv_dt():
         print(str.format('{0:.2f}',linf[i+1]*10**-maginfOrder),"\\cdot 10^{", str.format('{0:n}',maginfOrder),end=' ')
         print("}$ & ",str.format('{0:.2f}',linfOrder[i])," \\\\")
         print("\\hline")
-"""
+
 def initConditionsFlux(theta,z):
     a=4
     factor = pi/a/2
@@ -320,7 +320,7 @@ initCondsF = np.vectorize(initConditionsFlux, otypes=[np.float])
 
 def iota0(r = 6.0):
     return np.full_like(r,0.0,dtype=float)
-"""
+
 @pytest.mark.serial
 def test_fluxAdvection():
     dt=0.1
@@ -416,7 +416,7 @@ def test_fluxAdvection():
         print(str.format('{0:.2f}',linf[i+1]*10**-maginfOrder),"\\cdot 10^{", str.format('{0:n}',maginfOrder),end=' ')
         print("}$ & ",str.format('{0:.2f}',linfOrder[i])," \\\\")
         print("\\hline")
-"""
+
 def iota8(r = 6.0):
     return np.full_like(r,0.8,dtype=float)
 
@@ -465,12 +465,11 @@ def test_fluxAdvectionAligned():
         
         fluxAdv = FluxSurfaceAdvection(eta_vals, bsplines, layout, dt, iota8, zDegree=3)
         
-        #~ print(eta_vals[1])
-        
-        m, n = (4, 5)
+        m, n = (5, 4)
         theta = eta_grids[0]
         phi = eta_grids[1]*2*pi/domain[1][1]
-        f_vals[:,:] = 0.5 + 0.5 * np.sin( m*theta[:,None] + n*phi[None,:] )
+        f_vals[:,:] = 0.5 + 0.5 * np.sin( m*theta[:,None] - n*phi[None,:] )
+        
         final_f_vals = f_vals.copy()
         
         f_max = np.max(f_vals)
@@ -480,16 +479,6 @@ def test_fluxAdvectionAligned():
             fluxAdv.step(f_vals,0)
             f_max = np.max([f_max,np.max(f_vals)])
             f_min = np.min([f_min,np.min(f_vals)])
-        
-        #~ print(f_min,f_max)
-        
-        #~ print(np.min((f_vals-final_f_vals)**2),np.max((f_vals-final_f_vals)**2))
-        
-        #~ print(np.min(trapz((f_vals-final_f_vals)**2,eta_vals[1],axis=0)),np.max(trapz((f_vals-final_f_vals)**2,eta_vals[1],axis=0)))
-        #~ print(trapz((f_vals-final_f_vals)**2,eta_vals[1],axis=0))
-        
-        #~ print(trapz(trapz((f_vals-final_f_vals)**2,eta_vals[1],axis=0),eta_vals[2]))
-        #~ print(eta_vals[2])
         
         linf[i]=np.linalg.norm((f_vals-final_f_vals).flatten(),np.inf)
         l2[i]=np.sqrt(trapz(trapz((f_vals-final_f_vals)**2,eta_vals[1],axis=0),eta_vals[2]))
@@ -531,7 +520,7 @@ def test_fluxAdvectionAligned():
         print(str.format('{0:.2f}',linf[i+1]*10**-maginfOrder),"\\cdot 10^{", str.format('{0:n}',maginfOrder),end=' ')
         print("}$ & ",str.format('{0:.2f}',linfOrder[i])," \\\\")
         print("\\hline")
-"""
+
 def Phi(theta,z):
     #return np.cos(z*pi*0.1) + np.sin(theta)
     return np.sin(z*pi*0.1)**2 + np.cos(theta)**2
@@ -672,4 +661,4 @@ def test_Phi_deriv_dz():
         print(str.format('{0:.2f}',linf[i]*10**-maginfOrder),"\\cdot 10^{", str.format('{0:n}',maginfOrder),end=' ')
         print("}$ & ",str.format('{0:.2f}',linfOrder[i-1])," \\\\")
         print("\\hline")
-"""
+
