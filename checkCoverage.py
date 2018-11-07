c2fbc27c1556655da122a67a9d713c89c5dbfb9d
ebourne <louise.bourne@gmail.com>
2018-11-07 21:36:52 +0100

    Combine -m parallel and -m serial into one command (no -m) and use this command directly for the coverage tests (fewer repeated tests decreases the chances of failing on the splines). Output added for coverage test to see where tests fail

--- checkCoverage.py
+++ checkCoverage.py
@@ -1,12 +1,23 @@
 
+bashCommand = "py.test-3 pygyro/initialisation --cov=pygyro --cov-report=term"
+import subprocess
+process = subprocess.run(bashCommand.split(), check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-search = open("current_coverage.txt","r")
+out = str(process.stdout)
+out = out[2:-1]
+out = out.replace("\\n","\n")
 
-for line in search:
-    if ("TOTAL" in line):
-        i=line.rfind(" ")
-        j=line.rfind("%")
-        pc=float(line[i:j])
-        assert(pc>=75)
+err = str(process.stderr)
+err = err[2:-1]
+err = err.replace("\\n","\n")
 
-search.close()
+print(out)
+
+print(err)
+
+if (process.returncode==0):
+    i = out.find("TOTAL")
+    totline = out[i:]
+    totline = totline[:totline.find("\n")]
+    pc=float(totline[totline.rfind(" "):totline.rfind("%")])
+    assert(pc>=75)
