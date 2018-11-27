a7088d95a3608d6fe90bf406c511d8661e6d11ae
EmilyBourne <louise.bourne@gmail.com>
2018-11-27 14:24:49 +0100

    Reduce number of tests. Correctly redirect output even if fail

--- checkCoverage.py
+++ checkCoverage.py
@@ -1,31 +1,42 @@
 from shlex  import split
 import subprocess
+import os
 
-bashCommand = "pytest pygyro --cov=pygyro --cov-report=term -k='not long'"
+bashCommand = "pytest pygyro/advection --cov=pygyro --cov-report=term -k='not long' > cov_out.txt 2> cov_err.txt"
 try:
-    process = subprocess.run(split(bashCommand), check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.run(bashCommand, shell=True, check=True)
 except:
-    out = str(process.stdout)
+    file = open("cov_out.txt", "r") 
+    out = file.read()
+    file.close()
+    os.remove("cov_out.txt")
     out = out[2:-1]
     out = out.replace("\\n","\n")
     print(out)
     
-    err = str(process.stderr)
+    file = open("cov_err.txt", "r") 
+    err = file.read()
+    file.close()
+    os.remove("cov_err.txt")
     err = err[2:-1]
     err = err.replace("\\n","\n")
     print(err)
     raise
 
-out = str(process.stdout)
+file = open("cov_out.txt", "r") 
+out = file.read()
+file.close()
+os.remove("cov_out.txt")
 out = out[2:-1]
 out = out.replace("\\n","\n")
-
-err = str(process.stderr)
-err = err[2:-1]
-err = err.replace("\\n","\n")
-
 print(out)
 
+file = open("cov_err.txt", "r") 
+err = file.read()
+file.close()
+os.remove("cov_err.txt")
+err = err[2:-1]
+err = err.replace("\\n","\n")
 print(err)
 
 if (process.returncode==0):
