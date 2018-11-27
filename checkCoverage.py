6133f7faf3f7f8ac036bbbcace03345e31033158
EmilyBourne <louise.bourne@gmail.com>
2018-11-27 14:27:30 +0100

    Neaten output

--- checkCoverage.py
+++ checkCoverage.py
@@ -21,27 +21,26 @@ except:
     err = err[2:-1]
     err = err.replace("\\n","\n")
     print(err)
-    raise
+else:
+    file = open("cov_out.txt", "r") 
+    out = file.read()
+    file.close()
+    os.remove("cov_out.txt")
+    out = out[2:-1]
+    out = out.replace("\\n","\n")
+    print(out)
 
-file = open("cov_out.txt", "r") 
-out = file.read()
-file.close()
-os.remove("cov_out.txt")
-out = out[2:-1]
-out = out.replace("\\n","\n")
-print(out)
+    file = open("cov_err.txt", "r") 
+    err = file.read()
+    file.close()
+    os.remove("cov_err.txt")
+    err = err[2:-1]
+    err = err.replace("\\n","\n")
+    print(err)
 
-file = open("cov_err.txt", "r") 
-err = file.read()
-file.close()
-os.remove("cov_err.txt")
-err = err[2:-1]
-err = err.replace("\\n","\n")
-print(err)
-
-if (process.returncode==0):
-    i = out.find("TOTAL")
-    totline = out[i:]
-    totline = totline[:totline.find("\n")]
-    pc=float(totline[totline.rfind(" "):totline.rfind("%")])
-    assert(pc>=75)
+    if (process.returncode==0):
+        i = out.find("TOTAL")
+        totline = out[i:]
+        totline = totline[:totline.find("\n")]
+        pc=float(totline[totline.rfind(" "):totline.rfind("%")])
+        assert(pc>=75)
