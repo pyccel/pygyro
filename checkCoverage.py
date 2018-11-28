5509cc88f735456608a0e6c91b38ec566f2d6de3
ebourne <louise.bourne@gmail.com>
2018-11-28 14:02:48 +0100

    Use better checkCoverage.py file from numba branch

--- checkCoverage.py
+++ checkCoverage.py
@@ -1,23 +1,46 @@
-
-bashCommand = "pytest pygyro --cov=pygyro --cov-report=term"
+from shlex  import split
 import subprocess
-process = subprocess.run(bashCommand.split(), check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+import os
 
-out = str(process.stdout)
-out = out[2:-1]
-out = out.replace("\\n","\n")
+bashCommand = "pytest pygyro --cov=pygyro --cov-report=term -k='not long' > cov_out.txt 2> cov_err.txt"
+try:
+    process = subprocess.run(bashCommand, shell=True, check=True)
+except:
+    file = open("cov_out.txt", "r") 
+    out = file.read()
+    file.close()
+    os.remove("cov_out.txt")
+    out = out[2:-1]
+    out = out.replace("\\n","\n")
+    print(out)
+    
+    file = open("cov_err.txt", "r") 
+    err = file.read()
+    file.close()
+    os.remove("cov_err.txt")
+    err = err[2:-1]
+    err = err.replace("\\n","\n")
+    print(err)
+else:
+    file = open("cov_out.txt", "r") 
+    out = file.read()
+    file.close()
+    os.remove("cov_out.txt")
+    out = out[2:-1]
+    out = out.replace("\\n","\n")
+    print(out)
 
-err = str(process.stderr)
-err = err[2:-1]
-err = err.replace("\\n","\n")
+    file = open("cov_err.txt", "r") 
+    err = file.read()
+    file.close()
+    os.remove("cov_err.txt")
+    err = err[2:-1]
+    err = err.replace("\\n","\n")
+    print(err)
 
-print(out)
-
-print(err)
-
-if (process.returncode==0):
-    i = out.find("TOTAL")
-    totline = out[i:]
-    totline = totline[:totline.find("\n")]
-    pc=float(totline[totline.rfind(" "):totline.rfind("%")])
-    assert(pc>=75)
+    if (process.returncode==0):
+        i = out.find("TOTAL")
+        totline = out[i:]
+        totline = totline[:totline.find("\n")]
+        pc=float(totline[totline.rfind(" "):totline.rfind("%")])
+        assert(pc>=75)
