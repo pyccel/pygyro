949ba8e464835f42b2b2ce4710afdc9d71b882c5
ebourne <louise.bourne@gmail.com>
2018-12-18 15:14:03 +0100

    Added Plotter2d tests

--- pygyro/utilities/visual_test_plots.py
+++ pygyro/utilities/visual_test_plots.py
@@ -66,3 +66,24 @@ def test_3DPlot():
 
     comm.Barrier()
 
+@pytest.mark.parametrize( "dim1,dim2,lab1,lab2", [(0,1,'r','θ'),(0,2,'r','z'),(0,3,'r','v'),(1,2,'θ','z'),(1,3,'θ','v'),(2,3,'z','v')] )
+def test_2D(dim1,dim2,lab1,lab2):
+    npts=[10,20,10,10]
+    comm=MPI.COMM_WORLD
+    size=comm.Get_size()
+    
+    grid,constants,tStart=setupCylindricalGrid('flux_surface',npts = npts)
+
+    p = Plotter2d(grid,dim1,dim2,False)
+    p.setLabels(lab1,lab2)
+    p.show()
+
+def test_2D_periodic():
+    npts=[10,20,10,10]
+    comm=MPI.COMM_WORLD
+    size=comm.Get_size()
+    
+    grid,constants,tStart=setupCylindricalGrid('flux_surface',npts = npts)
+
+    p = Plotter2d(grid,0,1,True)
+    p.show()
