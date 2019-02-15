e783ea218593ebe4d65c2b43b1874c9b32ecdf17
Yaman Güçlü <yaman.guclu@gmail.com>
2019-02-15 18:34:10 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+import pygyro
+
+#==============================================================================
+
+NAME     = 'pygyro'
+VERSION  =  pygyro.__version__
+AUTHOR   = 'Emily Bourne, Yaman Güçlü'
+EMAIL    = 'yaman.guclu@gmail.com'
+URL      = 'https://github.com/pyccel/pygyro'
+DESCR    = 'Python library for parallel gyro-kinetic simulations'
+KEYWORDS = ['Semi-Lagrangian', 'MPI', 'Gyrokinetic']
+#LICENSE  = 'LICENSE.txt'
+
+#==============================================================================
+
+setup(
+    name             = NAME,
+    version          = VERSION,
+    author           = AUTHOR,
+    author_email     = EMAIL,
+    description      = DESCR,
+    long_description = open('README').read(),
+#    license          = LICENSE,
+    keywords         = KEYWORDS,
+    url              = URL,
+    packages         = find_packages(),
+    classifiers      = [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+#        "License :: OSI Approved :: BSD License",
+    ],
+)
+
+if __name__ == '__main__':
+    setup()
