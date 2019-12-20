87767f356d80d5c6b221c26325a00585fcb5c732
EmilyBourne <louise.bourne@gmail.com>
2019-12-20 17:50:10 +0100

    Add cython convertible files (basic, without type indicators)

---
+++ pygyro/initialisation/initialiser_func.pyx
@@ -0,0 +1,25 @@
+from .mod_initialiser_funcs     import fEq,perturbation
+
+def init_f(r,theta,z,vPar,m,n, eps,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi,deltaR,R0):
+    return fEq(r,vPar,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi)*(1+eps*perturbation(r,theta,z,m,n,rp,deltaR,R0))
+
+def init_f_flux(surface,r,theta,zVec,vPar,m,n, eps,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi,deltaR,R0):
+    for i,q in enumerate(theta):
+        for j,z in enumerate(zVec):
+            surface[i,j]=fEq(r,vPar,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi)*(1+eps*perturbation(r,q,z,m,n,rp,deltaR,R0))
+
+def init_f_pol(surface,rVec,theta,z,vPar,m,n, eps,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi,deltaR,R0):
+    for i,q in enumerate(theta):
+        for j,r in enumerate(rVec):
+            surface[i,j]=fEq(r,vPar,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi)*(1+eps*perturbation(r,q,z,m,n,rp,deltaR,R0))
+
+def init_f_vpar(surface,r,theta,z,vPar,m,n, eps,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi,deltaR,R0):
+    for i,q in enumerate(theta):
+        for j,v in enumerate(vPar):
+            surface[i,j]=fEq(r,v,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi)*(1+eps*perturbation(r,q,z,m,n,rp,deltaR,R0))
+
+def feq_vector(surface,r_vec,vPar,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi):
+    for i,r in enumerate(r_vec):
+        for j,v in enumerate(vPar):
+            surface[i,j]=fEq(r,v,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi)
+
