9eafd099bb99b0c56b2aa3ae9a1d9f3a2c5c6084
EmilyBourne <louise.bourne@gmail.com>
2020-02-12 17:27:09 +0100

    Use lower case names to have the same function names as are required for pyccel

--- pygyro/initialisation/numba_initialiser_funcs.py
+++ pygyro/initialisation/numba_initialiser_funcs.py
@@ -6,22 +6,22 @@ def n0(r,CN0,kN0,deltaRN0,rp):
     return CN0*exp(-kN0*deltaRN0*tanh((r-rp)/deltaRN0))
 
 @njit
-def Ti(r,CTi,kTi,deltaRTi,rp):
-    return CTi*exp(-kTi*deltaRTi*tanh((r-rp)/deltaRTi))
+def ti(r,Cti,kti,deltaRti,rp):
+    return Cti*exp(-kti*deltaRti*tanh((r-rp)/deltaRti))
 
 @njit
 def perturbation(r,theta,z,m,n,rp,deltaR,R0):
     return exp(-(r-rp)**2/deltaR)*cos(m*theta+n*z/R0)
 
 @njit
-def fEq(r,vPar,CN0,kN0,deltaRN0,rp,CTi,kTi,deltaRTi):
-    return n0(r,CN0,kN0,deltaRN0,rp)*exp(-0.5*vPar*vPar/Ti(r,CTi,kTi,deltaRTi,rp))/sqrt(2.0*pi*Ti(r,CTi,kTi,deltaRTi,rp))
+def f_eq(r,vPar,CN0,kN0,deltaRN0,rp,Cti,kti,deltaRti):
+    return n0(r,CN0,kN0,deltaRN0,rp)*exp(-0.5*vPar*vPar/ti(r,Cti,kti,deltaRti,rp))/sqrt(2.0*pi*ti(r,Cti,kti,deltaRti,rp))
 
 @njit
-def n0derivNormalised(r,kN0,rp,deltaRN0):
+def n0deriv_normalised(r,kN0,rp,deltaRN0):
     return -kN0 * (1 - tanh( (r - rp ) / deltaRN0 )**2)
 
 @njit
-def Te(r,CTe,kTe,deltaRTe,rp):
-    return CTe*exp(-kTe*deltaRTe*tanh((r-rp)/deltaRTe))
+def te(r,Cte,kte,deltaRte,rp):
+    return Cte*exp(-kte*deltaRte*tanh((r-rp)/deltaRte))
 
