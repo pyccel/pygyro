5ad84a67d221ed3e1704ad4f54ce282fdfd133a1
Dominik Bell <dominik.bell@ipp.mpg.de>
2022-07-19 16:48:22 +0200

    formatted files in /diagnostics

--- pygyro/diagnostics/diagnostic_collector.py
+++ pygyro/diagnostics/diagnostic_collector.py
@@ -1,33 +1,42 @@
-from mpi4py             import MPI
-import numpy            as np
+from mpi4py import MPI
+import numpy as np
 
-from ..model.grid       import Grid
-from .norms             import l2, l1, nParticles
-from .energy            import KineticEnergy
+from ..model.grid import Grid
+from .norms import l2, l1, nParticles
+from .energy import KineticEnergy
+
 
 class DiagnosticCollector:
-    def __init__( self, comm, saveStep: int, dt: float, distribFunc: Grid, phi: Grid ):
+    """
+    TODO
+    """
+
+    def __init__(self, comm, saveStep: int, dt: float, distribFunc: Grid, phi: Grid):
         self.saveStep = saveStep
         self.dt = dt
         self.comm = comm
         self.rank = comm.Get_rank()
 
-        self.diagnostics = np.zeros([8,saveStep])
-        self.l2PhiResult=np.zeros(saveStep)
-        self.l2GridResult=np.zeros(saveStep)
-        self.l1Result=np.zeros(saveStep)
-        self.nPartResult=np.zeros(saveStep)
-        self.min_val=np.zeros(saveStep)
-        self.max_val=np.zeros(saveStep)
-        self.KE_val=np.zeros(saveStep)
+        self.diagnostics = np.zeros([8, saveStep])
+        self.l2PhiResult = np.zeros(saveStep)
+        self.l2GridResult = np.zeros(saveStep)
+        self.l1Result = np.zeros(saveStep)
+        self.nPartResult = np.zeros(saveStep)
+        self.min_val = np.zeros(saveStep)
+        self.max_val = np.zeros(saveStep)
+        self.KE_val = np.zeros(saveStep)
 
-        self.l2_phi_class = l2(phi.eta_grid,phi.getLayout('v_parallel_2d'))
-        self.l1class = l1(distribFunc.eta_grid,distribFunc.getLayout('v_parallel'))
-        self.l2_grid_class = l2(distribFunc.eta_grid,distribFunc.getLayout('v_parallel'))
-        self.npart = nParticles(distribFunc.eta_grid,distribFunc.getLayout('v_parallel'))
-        self.KEclass = KineticEnergy(distribFunc.eta_grid,distribFunc.getLayout('v_parallel'))
+        self.l2_phi_class = l2(phi.eta_grid, phi.getLayout('v_parallel_2d'))
+        self.l1class = l1(distribFunc.eta_grid,
+                          distribFunc.getLayout('v_parallel'))
+        self.l2_grid_class = l2(distribFunc.eta_grid,
+                                distribFunc.getLayout('v_parallel'))
+        self.npart = nParticles(distribFunc.eta_grid,
+                                distribFunc.getLayout('v_parallel'))
+        self.KEclass = KineticEnergy(
+            distribFunc.eta_grid, distribFunc.getLayout('v_parallel'))
 
-    def collect ( self, f: Grid, phi: Grid, t: float):
+    def collect(self, f: Grid, phi: Grid, t: float):
         """
         Collect various diagnostics to be saved in the following order:
          0 - time
@@ -51,37 +60,54 @@ class DiagnosticCollector:
             The current time
         """
         ti = t//self.dt
-        idx = ti%self.saveStep
-        self.diagnostics[0,idx]=t
-        self.diagnostics[1,idx]=self.l2_phi_class.l2NormSquared(phi)
-        self.diagnostics[2,idx]=self.l2_grid_class.l2NormSquared(f)
-        self.diagnostics[3,idx]=self.l1class.l1Norm(f)
-        self.diagnostics[4,idx]=self.npart.getN(f)
-        self.diagnostics[5,idx]=f.getMin()
-        self.diagnostics[6,idx]=f.getMax()
-        self.diagnostics[7,idx]=self.KEclass.getKE(f)
+        idx = ti % self.saveStep
+        
+        self.diagnostics[0, idx] = t
+        self.diagnostics[1, idx] = self.l2_phi_class.l2NormSquared(phi)
+        self.diagnostics[2, idx] = self.l2_grid_class.l2NormSquared(f)
+        self.diagnostics[3, idx] = self.l1class.l1Norm(f)
+        self.diagnostics[4, idx] = self.npart.getN(f)
+        self.diagnostics[5, idx] = f.getMin()
+        self.diagnostics[6, idx] = f.getMax()
+        self.diagnostics[7, idx] = self.KEclass.getKE(f)
 
     def reduce(self):
-        self.comm.Reduce(self.diagnostics[1,:],self.l2PhiResult,op=MPI.SUM, root=0)
-        self.comm.Reduce(self.diagnostics[2,:],self.l2GridResult,op=MPI.SUM, root=0)
-        self.comm.Reduce(self.diagnostics[3,:],self.l1Result,op=MPI.SUM, root=0)
-        self.comm.Reduce(self.diagnostics[4,:],self.nPartResult,op=MPI.SUM, root=0)
-        self.comm.Reduce(self.diagnostics[5,:],self.min_val,op=MPI.MIN, root=0)
-        self.comm.Reduce(self.diagnostics[6,:],self.max_val,op=MPI.MAX, root=0)
-        self.comm.Reduce(self.diagnostics[7,:],self.KE_val,op=MPI.SUM, root=0)
+        """
+        TODO
+        """
+        self.comm.Reduce(self.diagnostics[1, :],
+                         self.l2PhiResult, op=MPI.SUM, root=0)
+        self.comm.Reduce(self.diagnostics[2, :],
+                         self.l2GridResult, op=MPI.SUM, root=0)
+        self.comm.Reduce(self.diagnostics[3, :],
+                         self.l1Result, op=MPI.SUM, root=0)
+        self.comm.Reduce(self.diagnostics[4, :],
+                         self.nPartResult, op=MPI.SUM, root=0)
+        self.comm.Reduce(self.diagnostics[5, :],
+                         self.min_val, op=MPI.MIN, root=0)
+        self.comm.Reduce(self.diagnostics[6, :],
+                         self.max_val, op=MPI.MAX, root=0)
+        self.comm.Reduce(self.diagnostics[7, :],
+                         self.KE_val, op=MPI.SUM, root=0)
         if (self.rank == 0):
             self.l2PhiResult = np.sqrt(self.l2PhiResult)
             self.l2GridResult = np.sqrt(self.l2GridResult)
 
     def __str__(self):
+        """
+        TODO
+        """
         myStr = []
         for i in range(self.saveStep):
             myStr.append(self.getLine(i))
             myStr.append('\n')
         return ''.join(myStr)
 
-    def getLine(self,i):
+    def getLine(self, i):
+        """
+        TODO
+        """
         return "{t:10g}   {l2P:16.10e}   {l2G:16.10e}   {l1:16.10e}   {np:16.10e}   {minim:16.10e}   {maxim:16.10e}   {ke:16.10e}". \
-                        format(t=self.diagnostics[0,i],l2P=self.l2PhiResult[i],l2G=self.l2GridResult[i],
-                                l1=self.l1Result[i], np = self.nPartResult[i],
-                                minim=self.min_val[i],maxim=self.max_val[i],ke=self.KE_val[i])
+            format(t=self.diagnostics[0, i], l2P=self.l2PhiResult[i], l2G=self.l2GridResult[i],
+                   l1=self.l1Result[i], np=self.nPartResult[i],
+                   minim=self.min_val[i], maxim=self.max_val[i], ke=self.KE_val[i])
--- pygyro/diagnostics/test_norms.py
+++ pygyro/diagnostics/test_norms.py
@@ -1,274 +1,307 @@
-from mpi4py     import MPI
-import numpy    as np
+from mpi4py import MPI
+import numpy as np
 import pytest
 
-from .norms                                     import l2, l1, nParticles
-from ..                                         import splines as spl
-from ..model.process_grid                       import compute_2d_process_grid_from_max
-from ..model.grid                               import Grid
-from ..model.layout                             import LayoutSwapper
-from ..initialisation.setups                    import setupCylindricalGrid
+from .norms import l2, l1, nParticles
+from .. import splines as spl
+from ..model.process_grid import compute_2d_process_grid_from_max
+from ..model.grid import Grid
+from ..model.layout import LayoutSwapper
+from ..initialisation.setups import setupCylindricalGrid
+
 
 def args_norm_phi():
-    for layout in ['v_parallel_2d','mode_solve','v_parallel_1d','poloidal']:
+    """
+    TODO
+    """
+    for layout in ['v_parallel_2d', 'mode_solve', 'v_parallel_1d', 'poloidal']:
         for _ in range(2):
             R0 = np.random.rand()*1000
             for _ in range(2):
                 rMin = np.random.rand()
                 for _ in range(2):
-                    rMax = np.random.randint(10,200)/10
-                    yield (layout, R0, rMin,rMax)
+                    rMax = np.random.randint(10, 200)/10
+                    yield (layout, R0, rMin, rMax)
+
 
 @pytest.mark.parallel
-@pytest.mark.parametrize( "layout,R0,rMin,rMax", args_norm_phi() )
-def test_l2Norm_is_volume(layout,R0,rMin,rMax):
+@pytest.mark.parametrize("layout,R0,rMin,rMax", args_norm_phi())
+def test_l2Norm_is_volume(layout, R0, rMin, rMax):
+    """
+    TODO
+    """
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    layout=comm.bcast(layout)
-    R0=comm.bcast(R0)
-    rMin=comm.bcast(rMin)
-    rMax=comm.bcast(rMax)
+    layout = comm.bcast(layout)
+    R0 = comm.bcast(R0)
+    rMin = comm.bcast(rMin)
+    rMax = comm.bcast(rMax)
 
-    npts = [10,11,12]
+    npts = [10, 11, 12]
 
-    domain    = [ [rMin,rMax], [0,2*np.pi], [0,R0*2*np.pi] ]
-    periodic  = [ False, True, True ]
-    nkts      = [n+1                           for n          in npts ]
-    breaks    = [np.linspace( *lims, num=num ) for (lims,num) in zip( domain, nkts )]
-    knots     = [spl.make_knots( b,3,p )       for (b,p)      in zip(breaks,periodic)]
-    bsplines  = [spl.BSplines( k,3,p )         for (k,p)      in zip(knots,periodic)]
-    eta_grid  = [bspl.greville                 for bspl       in bsplines]
+    domain = [[rMin, rMax], [0, 2*np.pi], [0, R0*2*np.pi]]
+    periodic = [False, True, True]
+    nkts = [n+1 for n in npts]
+    breaks = [np.linspace(*lims, num=num) for (lims, num) in zip(domain, nkts)]
+    knots = [spl.make_knots(b, 3, p) for (b, p) in zip(breaks, periodic)]
+    bsplines = [spl.BSplines(k, 3, p) for (k, p) in zip(knots, periodic)]
+    eta_grid = [bspl.greville for bspl in bsplines]
 
-    layout_poisson   = {'v_parallel_2d': [0,2,1],
-                        'mode_solve'   : [1,2,0]}
-    layout_vpar      = {'v_parallel_1d': [0,2,1]}
-    layout_poloidal  = {'poloidal'     : [2,1,0]}
+    layout_poisson = {'v_parallel_2d': [0, 2, 1],
+                      'mode_solve': [1, 2, 0]}
+    layout_vpar = {'v_parallel_1d': [0, 2, 1]}
+    layout_poloidal = {'poloidal': [2, 1, 0]}
 
-    nprocs = compute_2d_process_grid_from_max(min(npts[0],npts[1]),npts[2],size)
+    nprocs = compute_2d_process_grid_from_max(
+        min(npts[0], npts[1]), npts[2], size)
 
-    remapperPhi = LayoutSwapper( comm, [layout_poisson, layout_vpar, layout_poloidal],
-                                [nprocs,nprocs[0],nprocs[1]], eta_grid,
-                                layout )
+    remapperPhi = LayoutSwapper(comm, [layout_poisson, layout_vpar, layout_poloidal],
+                                [nprocs, nprocs[0], nprocs[1]], eta_grid,
+                                layout)
 
     theLayout = remapperPhi.getLayout(layout)
 
-    norm = l2(eta_grid,theLayout)
+    norm = l2(eta_grid, theLayout)
 
-    phi = Grid(eta_grid,bsplines,remapperPhi,layout,comm)
+    phi = Grid(eta_grid, bsplines, remapperPhi, layout, comm)
     phi._f[:] = 1
 
     l2Val = norm.l2NormSquared(phi)
     if (layout in layout_poisson):
-        l2Result = comm.reduce(l2Val,op=MPI.SUM, root=0)
+        l2Result = comm.reduce(l2Val, op=MPI.SUM, root=0)
     else:
-        comm = remapperPhi._managers[remapperPhi._handlers[layout]].communicators[0]
-        l2Result = comm.reduce(l2Val,op=MPI.SUM, root=0)
+        comm = remapperPhi._managers[remapperPhi._handlers[layout]
+                                     ].communicators[0]
+        l2Result = comm.reduce(l2Val, op=MPI.SUM, root=0)
 
+    if (rank == 0):
+        assert(abs(l2Result-((rMax**2-rMin**2)*np.pi*np.pi*R0*2)) < 5e-7)
 
-    if (rank==0):
-        assert(abs(l2Result-((rMax**2-rMin**2)*np.pi*np.pi*R0*2))<5e-7)
 
 def args_norm_grid():
-    for layout in ['poloidal','v_parallel','flux_surface','poloidal']:
+    """
+    TODO
+    """
+    for layout in ['poloidal', 'v_parallel', 'flux_surface', 'poloidal']:
         for _ in range(2):
             R0 = np.random.rand()*1000
             for _ in range(2):
                 rMin = np.random.rand()
                 for _ in range(2):
-                    rMax = np.random.randint(10,200)/10
+                    rMax = np.random.randint(10, 200)/10
                     for _ in range(2):
-                        vMax = np.random.randint(1,100)/10
-                        yield (layout, R0, rMin,rMax,vMax)
+                        vMax = np.random.randint(1, 100)/10
+                        yield (layout, R0, rMin, rMax, vMax)
+
 
 @pytest.mark.parallel
-@pytest.mark.parametrize( "layout,R0,rMin,rMax,vMax", args_norm_grid() )
-def test_l1Norm_grid_is_volume(layout,R0,rMin,rMax,vMax):
+@pytest.mark.parametrize("layout,R0,rMin,rMax,vMax", args_norm_grid())
+def test_l1Norm_grid_is_volume(layout, R0, rMin, rMax, vMax):
+    """
+    TODO
+    """
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    layout=comm.bcast(layout)
-    R0=comm.bcast(R0)
-    rMin=comm.bcast(rMin)
-    rMax=comm.bcast(rMax)
-    vMax=comm.bcast(vMax)
+    layout = comm.bcast(layout)
+    R0 = comm.bcast(R0)
+    rMin = comm.bcast(rMin)
+    rMax = comm.bcast(rMax)
+    vMax = comm.bcast(vMax)
 
-    npts = [10,11,12,13]
-    grid,_,_ = setupCylindricalGrid(npts   = npts,
-                                layout = layout,
-                                rMin = rMin,
-                                rMax = rMax,
-                                vMax = vMax,
-                                vMin = -vMax,
-                                zMax = 2*np.pi*R0,
-                                R0   = R0)
+    npts = [10, 11, 12, 13]
+    grid, _, _ = setupCylindricalGrid(npts=npts,
+                                      layout=layout,
+                                      rMin=rMin,
+                                      rMax=rMax,
+                                      vMax=vMax,
+                                      vMin=-vMax,
+                                      zMax=2*np.pi*R0,
+                                      R0=R0)
 
     theLayout = grid.getLayout(layout)
 
-    norm = l1(grid.eta_grid,theLayout)
+    norm = l1(grid.eta_grid, theLayout)
 
     grid._f[:] = 1
 
     l1Val = norm.l1Norm(grid)
-    l1Result = comm.reduce(l1Val,op=MPI.SUM, root=0)
+    l1Result = comm.reduce(l1Val, op=MPI.SUM, root=0)
 
-    if (rank==0):
+    if (rank == 0):
         # ~ print(l1Result,((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax))
-        assert(abs(l1Result-((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax))<5e-7)
+        assert(abs(l1Result-((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax)) < 5e-7)
+
 
 @pytest.mark.parallel
-@pytest.mark.parametrize( "layout,R0,rMin,rMax,vMax", args_norm_grid() )
-def test_l2Norm_grid_is_volume(layout,R0,rMin,rMax,vMax):
+@pytest.mark.parametrize("layout,R0,rMin,rMax,vMax", args_norm_grid())
+def test_l2Norm_grid_is_volume(layout, R0, rMin, rMax, vMax):
+    """
+    TODO
+    """
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    layout=comm.bcast(layout)
-    R0=comm.bcast(R0)
-    rMin=comm.bcast(rMin)
-    rMax=comm.bcast(rMax)
-    vMax=comm.bcast(vMax)
+    layout = comm.bcast(layout)
+    R0 = comm.bcast(R0)
+    rMin = comm.bcast(rMin)
+    rMax = comm.bcast(rMax)
+    vMax = comm.bcast(vMax)
 
-    npts = [10,11,12,13]
-    grid,_,_ = setupCylindricalGrid(npts   = npts,
-                                layout = layout,
-                                rMin = rMin,
-                                rMax = rMax,
-                                vMax = vMax,
-                                vMin = -vMax,
-                                zMax = 2*np.pi*R0,
-                                R0   = R0)
+    npts = [10, 11, 12, 13]
+    grid, _, _ = setupCylindricalGrid(npts=npts,
+                                      layout=layout,
+                                      rMin=rMin,
+                                      rMax=rMax,
+                                      vMax=vMax,
+                                      vMin=-vMax,
+                                      zMax=2*np.pi*R0,
+                                      R0=R0)
 
     theLayout = grid.getLayout(layout)
 
-    norm = l2(grid.eta_grid,theLayout)
+    norm = l2(grid.eta_grid, theLayout)
 
     grid._f[:] = 1
 
     l2Val = norm.l2NormSquared(grid)
-    l2Result = comm.reduce(l2Val,op=MPI.SUM, root=0)
+    l2Result = comm.reduce(l2Val, op=MPI.SUM, root=0)
 
-    if (rank==0):
+    if (rank == 0):
         # ~ print(l2Result,((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax))
-        assert(abs(l2Result-((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax))<5e-7)
+        assert(abs(l2Result-((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax)) < 5e-7)
+
 
 @pytest.mark.parallel
-@pytest.mark.parametrize( "layout,R0,rMin,rMax,vMax", args_norm_grid() )
-def test_l2Norm_grid_is_v(layout,R0,rMin,rMax,vMax):
+@pytest.mark.parametrize("layout,R0,rMin,rMax,vMax", args_norm_grid())
+def test_l2Norm_grid_is_v(layout, R0, rMin, rMax, vMax):
+    """
+    TODO
+    """
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    layout=comm.bcast(layout)
-    R0=comm.bcast(R0)
-    rMin=comm.bcast(rMin)
-    rMax=comm.bcast(rMax)
-    vMax=comm.bcast(vMax)
+    layout = comm.bcast(layout)
+    R0 = comm.bcast(R0)
+    rMin = comm.bcast(rMin)
+    rMax = comm.bcast(rMax)
+    vMax = comm.bcast(vMax)
 
-    npts = [10,11,12,13]
-    grid,_,_ = setupCylindricalGrid(npts   = npts,
-                                layout = layout,
-                                rMin = rMin,
-                                rMax = rMax,
-                                vMax = vMax,
-                                vMin = -vMax,
-                                zMax = 2*np.pi*R0,
-                                R0   = R0)
+    npts = [10, 11, 12, 13]
+    grid, _, _ = setupCylindricalGrid(npts=npts,
+                                      layout=layout,
+                                      rMin=rMin,
+                                      rMax=rMax,
+                                      vMax=vMax,
+                                      vMin=-vMax,
+                                      zMax=2*np.pi*R0,
+                                      R0=R0)
 
     theLayout = grid.getLayout(layout)
 
-    norm = l2(grid.eta_grid,theLayout)
+    norm = l2(grid.eta_grid, theLayout)
 
     idx_v = theLayout.inv_dims_order[3]
     v_range = grid.getGlobalIdxVals(idx_v)
 
-    reshaper = [slice(v_range.start,v_range.stop)]
+    reshaper = [slice(v_range.start, v_range.stop)]
     reshaper.extend([None]*(3-idx_v))
 
     grid._f[:] = grid.eta_grid[3][tuple(reshaper)]
 
     l2Val = norm.l2NormSquared(grid)
-    l2Result = comm.reduce(l2Val,op=MPI.SUM, root=0)
+    l2Result = comm.reduce(l2Val, op=MPI.SUM, root=0)
 
-    if (rank==0):
-        exactResult=(rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax**3/3
+    if (rank == 0):
+        exactResult = (rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax**3/3
         # ~ print(abs(l2Result-exactResult)/exactResult)
-        assert(abs(l2Result-exactResult)/exactResult<2e-2)
+        assert(abs(l2Result-exactResult)/exactResult < 2e-2)
+
 
 @pytest.mark.parallel
-@pytest.mark.parametrize( "layout,R0,rMin,rMax,vMax", args_norm_grid() )
-def test_NParticles_is_volume(layout,R0,rMin,rMax,vMax):
+@pytest.mark.parametrize("layout,R0,rMin,rMax,vMax", args_norm_grid())
+def test_NParticles_is_volume(layout, R0, rMin, rMax, vMax):
+    """
+    TODO
+    """
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
-    layout=comm.bcast(layout)
-    R0=comm.bcast(R0)
-    rMin=comm.bcast(rMin)
-    rMax=comm.bcast(rMax)
-    vMax=comm.bcast(vMax)
+    layout = comm.bcast(layout)
+    R0 = comm.bcast(R0)
+    rMin = comm.bcast(rMin)
+    rMax = comm.bcast(rMax)
+    vMax = comm.bcast(vMax)
 
-    npts = [10,11,12,13]
-    grid,_,_ = setupCylindricalGrid(npts   = npts,
-                                layout = layout,
-                                rMin = rMin,
-                                rMax = rMax,
-                                vMax = vMax,
-                                vMin = -vMax,
-                                zMax = 2*np.pi*R0,
-                                R0   = R0)
+    npts = [10, 11, 12, 13]
+    grid, _, _ = setupCylindricalGrid(npts=npts,
+                                      layout=layout,
+                                      rMin=rMin,
+                                      rMax=rMax,
+                                      vMax=vMax,
+                                      vMin=-vMax,
+                                      zMax=2*np.pi*R0,
+                                      R0=R0)
 
     theLayout = grid.getLayout(layout)
 
-    norm = nParticles(grid.eta_grid,theLayout)
+    norm = nParticles(grid.eta_grid, theLayout)
 
     grid._f[:] = 1
 
     l2Val = norm.getN(grid)
-    l2Result = comm.reduce(l2Val,op=MPI.SUM, root=0)
+    l2Result = comm.reduce(l2Val, op=MPI.SUM, root=0)
 
-    if (rank==0):
+    if (rank == 0):
         # ~ print(l2Result,((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax))
-        assert(abs(l2Result-((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax))<5e-7)
+        assert(abs(l2Result-((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax)) < 5e-7)
+
 
 @pytest.mark.parallel
-@pytest.mark.parametrize( "layout,R0,rMin,rMax,vMax", args_norm_grid() )
-def test_NParticles_grid_is_v(layout,R0,rMin,rMax,vMax):
+@pytest.mark.parametrize("layout,R0,rMin,rMax,vMax", args_norm_grid())
+def test_NParticles_grid_is_v(layout, R0, rMin, rMax, vMax):
+    """
+    TODO
+    """
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
-    layout=comm.bcast(layout)
-    R0=comm.bcast(R0)
-    rMin=comm.bcast(rMin)
-    rMax=comm.bcast(rMax)
-    vMax=comm.bcast(vMax)
+    layout = comm.bcast(layout)
+    R0 = comm.bcast(R0)
+    rMin = comm.bcast(rMin)
+    rMax = comm.bcast(rMax)
+    vMax = comm.bcast(vMax)
 
-    npts = [10,11,12,13]
-    grid,_,_ = setupCylindricalGrid(npts   = npts,
-                                layout = layout,
-                                rMin = rMin,
-                                rMax = rMax,
-                                vMax = vMax,
-                                vMin = -vMax,
-                                zMax = 2*np.pi*R0,
-                                R0   = R0)
+    npts = [10, 11, 12, 13]
+    grid, _, _ = setupCylindricalGrid(npts=npts,
+                                      layout=layout,
+                                      rMin=rMin,
+                                      rMax=rMax,
+                                      vMax=vMax,
+                                      vMin=-vMax,
+                                      zMax=2*np.pi*R0,
+                                      R0=R0)
 
     theLayout = grid.getLayout(layout)
 
-    norm = nParticles(grid.eta_grid,theLayout)
+    norm = nParticles(grid.eta_grid, theLayout)
 
     idx_v = theLayout.inv_dims_order[3]
     v_range = grid.getGlobalIdxVals(idx_v)
 
-    reshaper = [slice(v_range.start,v_range.stop)]
+    reshaper = [slice(v_range.start, v_range.stop)]
     reshaper.extend([None]*(3-idx_v))
 
     grid._f[:] = grid.eta_grid[3][tuple(reshaper)]
 
     l2Val = norm.getN(grid)
-    l2Result = comm.reduce(l2Val,op=MPI.SUM, root=0)
+    l2Result = comm.reduce(l2Val, op=MPI.SUM, root=0)
 
-    if (rank==0):
+    if (rank == 0):
         # ~ print(l2Result,((rMax**2-rMin**2)*np.pi*np.pi*R0*4*vMax))
-        assert(abs(l2Result)<5e-7)
+        assert(abs(l2Result) < 5e-7)
