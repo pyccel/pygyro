8a8a29a1da287a9f6013edcdd16857e292f2e6fc
Dominik Bell <dominik.bell@ipp.mpg.de>
2022-07-19 17:37:26 +0200

    formatted all files inside /splines

--- pygyro/splines/tests/analytical_profiles_2d.py
+++ pygyro/splines/tests/analytical_profiles_2d.py
@@ -8,35 +8,58 @@ from .analytical_profiles_base import AnalyticalProfile
 
 __all__ = ['AnalyticalProfile2D_CosCos']
 
-#===============================================================================
-class AnalyticalProfile2D_CosCos( AnalyticalProfile ):
+# ===============================================================================
 
-    def __init__( self, n1=1, n2=1, c1=0.0, c2=0.0 ):
-        twopi      = 2.0*math.pi
-        self._k1   = twopi * n1
-        self._k2   = twopi * n2
+
+class AnalyticalProfile2D_CosCos(AnalyticalProfile):
+    """
+    TODO
+    """
+
+    def __init__(self, n1=1, n2=1, c1=0.0, c2=0.0):
+        """
+        TODO
+        """
+        twopi = 2.0*math.pi
+        self._k1 = twopi * n1
+        self._k2 = twopi * n2
         self._phi1 = twopi * c1
         self._phi2 = twopi * c2
 
     @property
-    def ndims( self ):
+    def ndims(self):
+        """
+        TODO
+        """
         return 2
 
     @property
-    def domain( self ):
-        return (0.,1.), (0.,1.)
+    def domain(self):
+        """
+        TODO
+        """
+        return (0., 1.), (0., 1.)
 
     @property
-    def poly_order( self ):
-        return (-1,-1)
+    def poly_order(self):
+        """
+        TODO
+        """
+        return (-1, -1)
 
-    def eval( self, x, diff=[0,0] ):
+    def eval(self, x, diff=[0, 0]):
+        """
+        TODO
+        """
         x1, x2 = x
         d1, d2 = diff
         halfpi = 0.5*math.pi
-        return self._k1**d1 * np.cos( halfpi*d1 + self._k1*x1 + self._phi1 ) \
-             * self._k2**d2 * np.cos( halfpi*d2 + self._k2*x2 + self._phi2 )
+        return self._k1**d1 * np.cos(halfpi*d1 + self._k1*x1 + self._phi1) \
+            * self._k2**d2 * np.cos(halfpi*d2 + self._k2*x2 + self._phi2)
 
-    def max_norm( self, diff=[0,0] ):
+    def max_norm(self, diff=[0, 0]):
+        """
+        TODO
+        """
         d1, d2 = diff
         return self._k1**d1 * self._k2**d2
--- pygyro/splines/tests/utilities.py
+++ pygyro/splines/tests/utilities.py
@@ -3,45 +3,51 @@
 
 import numpy as np
 
-#===============================================================================
-def horner( x, *poly_coeffs ):
+# ===============================================================================
+
+
+def horner(x, *poly_coeffs):
     """ Use Horner's Scheme to evaluate a polynomial
         of coefficients *poly_coeffs at location x.
     """
     p = 0
     for c in poly_coeffs[::-1]:
-        p = p*x + c
+        p = p * x + c
     return p
 
-#===============================================================================
-def random_grid( domain, ncells, random_fraction ):
+# ===============================================================================
+
+
+def random_grid(domain, ncells, random_fraction):
     """ Create random grid over 1D domain with given number of cells.
     """
     # Create uniform grid on [0,1]
-    x = np.linspace( 0.0, 1.0, ncells+1 )
+    x = np.linspace(0.0, 1.0, ncells+1)
 
     # Apply random displacement to all points, then sort grid
-    x += (np.random.random_sample( ncells+1 )-0.5) * (random_fraction/ncells)
+    x += (np.random.random_sample(ncells+1)-0.5) * (random_fraction/ncells)
     x.sort()
 
     # Apply linear transformation y=m*x+q to match domain limits
     xa, xb = x[0], x[-1]
     ya, yb = domain
-    m = (   yb-ya   )/(xb-xa)
-    q = (xb*ya-xa*yb)/(xb-xa)
-    y = m*x + q
+    m = (yb - ya) / (xb - xa)
+    q = (xb * ya - xa * yb) / (xb - xa)
+    y = m * x + q
 
     # Avoid possible round-off
     y[0], y[-1] = domain
 
     return y
 
-#===============================================================================
-def falling_factorial( x, n ):
-  """ Calculate falling factorial of x.
-      [https://en.wikipedia.org/wiki/Falling_and_rising_factorials]
-  """
-  c = 1
-  for k in range(n):
-      c *= x-k
-  return c
+# ===============================================================================
+
+
+def falling_factorial(x, n):
+    """ Calculate falling factorial of x.
+        [https://en.wikipedia.org/wiki/Falling_and_rising_factorials]
+    """
+    c = 1
+    for k in range(n):
+        c *= x - k
+    return c
