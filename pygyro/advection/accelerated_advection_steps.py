aae6adc30e740f54173fb659aa0c9d751cbd342d
EmilyBoune <louise.bourne@gmail.com>
2022-07-27 13:05:36 +0200

    Correct angle bug

--- pygyro/advection/accelerated_advection_steps.py
+++ pygyro/advection/accelerated_advection_steps.py
@@ -278,6 +278,8 @@ def poloidal_advection_step_impl(f: 'float[:,:]', dt: 'float', v: 'float', rPts:
                     endPts_k2_r[i, j] = rMax
 
                 diff = abs(endPts_k2_q[i, j]-endPts_k1_q[i, j])
+                if diff > pi:
+                    diff = 2*pi-diff
                 if (diff > norm):
                     norm = diff
                 diff = abs(endPts_k2_r[i, j]-endPts_k1_r[i, j])
