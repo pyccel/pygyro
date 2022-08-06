fixes sign error

pygyro/arakawa/discrete_brackets_polar.py
@@ -307,6 +307,8 @@ def assemble_awk_bracket_dirichlet(phi, grid_theta, grid_r):
             data.append(coef)
 
     # Treatment of the left boundary
+    # -Coefficient in the following in order to keep the order of variables
+    # in sync with the paper calculations (note j.T = -j)
     i0 = 0
     for i1 in range(N_theta):
         ii = ind_to_tp_ind(i0, i1, N_r)
@@ -335,37 +337,37 @@ def assemble_awk_bracket_dirichlet(phi, grid_theta, grid_r):
         coef = br1 + br2 + br3 + br4 + br5
         row.append(ii)
         col.append(neighbor_index(0, 0, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i+1,0
         coef = br1
         row.append(ii)
         col.append(neighbor_index(0, 1, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i-1,0
         coef = br2
         row.append(ii)
         col.append(neighbor_index(0, -1, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i,1
         coef = br3
         row.append(ii)
         col.append(neighbor_index(1, 0, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i+1,1
         coef = br4
         row.append(ii)
         col.append(neighbor_index(1, 1, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i-1,1
         coef = br5
         row.append(ii)
         col.append(neighbor_index(1, -1, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
     # Treatment of the right boundary
     i0 = N_r-1
@@ -396,37 +398,37 @@ def assemble_awk_bracket_dirichlet(phi, grid_theta, grid_r):
         coef = br1 + br2 + br3 + br4 + br5
         row.append(ii)
         col.append(neighbor_index(0, 0, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i+1,N-1
         coef = br1
         row.append(ii)
         col.append(neighbor_index(0, 1, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i-1,N-1
         coef = br2
         row.append(ii)
         col.append(neighbor_index(0, -1, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i,N-2
         coef = br3
         row.append(ii)
         col.append(neighbor_index(-1, 0, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i-1,N-2
         coef = br4
         row.append(ii)
         col.append(neighbor_index(-1, -1, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
         # f_i+1,N-2
         coef = br5
         row.append(ii)
         col.append(neighbor_index(-1, 1, i0, i1, N_theta, N_r))
-        data.append(coef)
+        data.append(-coef)
 
     row = np.array(row)
     col = np.array(col)