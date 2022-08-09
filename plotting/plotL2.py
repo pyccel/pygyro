ae29956222b74940be4c56f231ad63a23c5c7679
EmilyBoune <louise.bourne@gmail.com>
2022-08-09 13:42:01 +0200

    Add folder as an argument

--- plotting/plotL2.py
+++ plotting/plotL2.py
@@ -1,7 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+import os
 
-data = np.loadtxt('phiDat.txt')
+parser = argparse.ArgumentParser(description='Plot the l2 norm of phi as a function of time')
+parser.add_argument('foldername', type=str, help='the name of the folder from which to load')
+
+args = parser.parse_args()
+
+data = np.loadtxt(os.path.join(args.foldername, 'phiDat.txt'))
 
 t = data[:,0]
 l2 = data[:,1]
