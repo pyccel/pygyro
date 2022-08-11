80312498ce1ae4d4147effb4361fa7fc649c2493
Emily.Bourne <louise.bourne@gmail.com>
2022-08-11 10:22:45 +0200

    Use pygyro tools to get correct points

--- plotting/plot_poloidal_slice.py
+++ plotting/plot_poloidal_slice.py
@@ -1,36 +1,80 @@
 import h5py
-import matplotlib.pyplot as plt
 import numpy as np
-import sys
+import matplotlib.pyplot    as plt
+from matplotlib             import rc        as pltFont
+import argparse
 import os
 
-filename = sys.argv[1]
-f = h5py.File(filename, 'r')
-data = np.array(f['dset'])
-f.close()
+from pygyro.tools.getSlice          import get_grid_slice
+from pygyro.tools.getPhiSlice       import get_phi_slice
+from pygyro.initialisation          import constants
+from pygyro                         import splines as spl
+
+parser = argparse.ArgumentParser(description='Process filename')
+parser.add_argument('filename', metavar='filename',nargs=1,type=str,
+                    default=[""],
+                   help='the name of the folder from which to load and in which to save')
+
+args = parser.parse_args()
+
+filename = args.filename[0]
+foldername = os.path.dirname(filename)
+
+t = int(filename[filename.rfind('_')+1:filename.rfind('.')])
+print("t=",t)
+
+file = h5py.File(filename,'r')
+dataset=file['/dset']
+
+shape = dataset.shape
+data_shape = list(shape)
+data_shape[0]+=1
+
+data = np.ndarray(data_shape)
+
+data[:-1,:]=dataset[:]
+data[0,:]=data[0,:]
+
+if (len(shape)==3):
+    get_phi_slice(foldername,t)
+    file = h5py.File(filename,'r')
+    dataset=file['/dset']
+elif (len(shape)==4):
+    get_grid_slice(foldername,t)
+
+file.close()
+
+npts = shape[::-1]
+degree = [3,3]
+period = [False,True]
+domain = [[0.1, 14.5],[0,2*np.pi]]
+
+nkts      = [n+1+d*(int(p)-1)              for (n,d,p)    in zip( npts,degree, period )]
+breaks    = [np.linspace( *lims, num=num ) for (lims,num) in zip( domain, nkts )]
+knots     = [spl.make_knots( b,d,p )       for (b,d,p)    in zip( breaks, degree, period )]
+bsplines  = [spl.BSplines( k,d,p )         for (k,d,p)    in zip(  knots, degree, period )]
+eta_grid  = [bspl.greville                 for bspl       in bsplines]
+
+theta = np.repeat(np.append(eta_grid[1], 2*np.pi), npts[0]) \
+                    .reshape(npts[1]+1, npts[0])
+r = np.tile(eta_grid[0], npts[1]+1) \
+        .reshape(npts[1]+1, npts[0])
 
 
-t = os.path.splitext(os.path.basename(filename))[0].split('_')[1]
+x=r*np.cos(theta)
+y=r*np.sin(theta)
 
-nq, nr = data.shape
+font = {'size'   : 16}
 
-r = np.linspace(0.1, 14.5, nr)
-q = np.linspace(0, 2*np.pi, nq, endpoint=False)
+pltFont('font', **font)
+fig = plt.figure()
+ax = fig.add_axes([0.1, 0.2, 0.7, 0.7],)
+ax.set_title('T = {}'.format(t))
+colorbarax2 = fig.add_axes([0.85, 0.15, 0.03, 0.8],)
+plot = ax.pcolormesh(x,y,data,cmap="jet")
+fig.colorbar(plot,cax=colorbarax2)
 
-ax = plt.subplot(111, projection='polar')
-
-data_min = data.min()
-data_max = data.max()
-if np.sign(data_min) != np.sign(data_max):
-    bound = max(data_max, -data_min)
-    clevels = np.linspace( -bound, bound, 101)
-else:
-    clevels = np.linspace( data_min, data_max, 101)
-im = ax.contourf( q, r, data.T, clevels, cmap='seismic' )
-#im = ax.contourf( q, r, data.T, clevels, cmap='jet' )
-for c in im.collections:
-    c.set_edgecolor('face')
-plt.colorbar( im )
-plt.title(f"t = {t}")
-
+ax.set_xlabel("x [m]")
+ax.set_ylabel("y [m]")
+#~ plt.savefig('debug_results/WO-Flux{}'.format(filename[filename.rfind('/'):-3]))
 plt.show()
