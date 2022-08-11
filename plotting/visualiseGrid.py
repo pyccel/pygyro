cf363c343e9534a33614aa5c9f9681c750942895
Emily.Bourne <louise.bourne@gmail.com>
2022-08-11 12:28:39 +0200

    Keep both options

--- plotting/visualiseGrid.py
+++ plotting/visualiseGrid.py
@@ -2,20 +2,22 @@ from mpi4py import MPI
 import sys
 
 from pygyro.utilities.grid_plotter import SlicePlotterNd
-from pygyro.initialisation.setups import setupCylindricalGrid
-
-foldername = sys.argv[1]
+from pygyro.initialisation.setups import setupCylindricalGrid, setupFromFile
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-# distribFunc, constants, t = setupCylindricalGrid(constantFile='iota0.json',
-#                                                 layout='v_parallel',
-#                                                 comm=comm,
-#                                                 allocateSaveMemory=True)
-distribFunc, constants, t = setupFromFile(foldername, comm=comm,
-                                          allocateSaveMemory=True,
-                                          timepoint=tEnd)
+if len(sys.argv == 1):
+    foldername = sys.argv[1]
+
+    distribFunc, constants, t = setupFromFile(foldername, comm=comm,
+                                              allocateSaveMemory=True,
+                                              timepoint=tEnd)
+else:
+    print("Showing initialisation with default values")
+    distribFunc, constants, t = setupCylindricalGrid(layout='v_parallel',
+                                                     comm=comm,
+                                                     allocateSaveMemory=True)
 
 plotter = SlicePlotterNd(distribFunc, 0, 1, True, sliderDimensions=[
                          2, 3], sliderNames=['z', 'v'])
