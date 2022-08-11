184f1d5abac89c2bd3f8974dfa5f9ab448c52df9
Emily.Bourne <louise.bourne@gmail.com>
2022-08-11 12:22:42 +0200

    Pep8

--- plotting/visualiseGrid.py
+++ plotting/visualiseGrid.py
@@ -9,13 +9,13 @@ foldername = sys.argv[1]
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-#distribFunc, constants, t = setupCylindricalGrid(constantFile='iota0.json',
+# distribFunc, constants, t = setupCylindricalGrid(constantFile='iota0.json',
 #                                                 layout='v_parallel',
 #                                                 comm=comm,
 #                                                 allocateSaveMemory=True)
-distribFunc, constants, t = setupFromFile(foldername, comm = comm,
-                            allocateSaveMemory = True,
-                            timepoint = tEnd)
+distribFunc, constants, t = setupFromFile(foldername, comm=comm,
+                                          allocateSaveMemory=True,
+                                          timepoint=tEnd)
 
 plotter = SlicePlotterNd(distribFunc, 0, 1, True, sliderDimensions=[
                          2, 3], sliderNames=['z', 'v'])
