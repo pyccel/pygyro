20b1d69a2f71d42f2c362b586cc144538b958004
Dominik Bell <dominik.bell@ipp.mpg.de>
2022-11-11 12:37:12 +0100

    Add submit script

---
+++ scripts/submit_cobra.cmd
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+# Standard output and error:
+#SBATCH -o ./tjob.out.%j
+#SBATCH -e ./tjob.err.%j
+# Initial working directory:
+#SBATCH -D ./
+# Job Name:
+#SBATCH -J pygyro-pp
+# Queue (Partition):
+#SBATCH --partition=express
+# Number of nodes and MPI tasks per node:
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=85000
+#
+#SBATCH --mail-type=begin,end,fail
+#SBATCH --mail-user=dominik.bell@ipp.mpg.de
+#
+# Wall clock limit:
+#SBATCH --time=00:30:00
+
+module purge
+module load intel/21.5.0
+module load anaconda/3/2021.11
+module load impi/2021.5
+module load mpi4py
+module load h5py-mpi
+module load gcc/11
+module list
+
+
+# Run the program:
+srun python3 fullSimulation.py 8000 1790 -c testSetups/iota0.json > prog.out
