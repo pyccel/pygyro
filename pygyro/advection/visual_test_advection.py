69e8efc92b221a1369d51d8ce5a855d3e960c0f9
EmilyBoune <louise.bourne@gmail.com>
2023-01-02 16:58:14 +0100

    Fix visual test

--- pygyro/advection/visual_test_advection.py
+++ pygyro/advection/visual_test_advection.py
@@ -31,7 +31,7 @@ def test_fluxSurfaceAdvection():
     nkts = [n+1 for n in npts]
     breaks = [np.linspace(*lims, num=num) for (lims, num) in zip(domain, nkts)]
     knots = [spl.make_knots(b, 3, True) for b in breaks]
-    bsplines = [spl.BSplines(k, 3, True) for k in knots]
+    bsplines = [spl.BSplines(k, 3, True, True) for k in knots]
     eta_grids = [bspl.greville for bspl in bsplines]
 
     eta_vals[1] = eta_grids[0]
@@ -85,7 +85,7 @@ def test_poloidalAdvection_invariantPhi():
     N = 200
     dt = 0.1
 
-    v = 0
+    v = 0.0
 
     f_vals = np.ndarray([N+1, npts[1], npts[0]])
 
@@ -96,7 +96,7 @@ def test_poloidalAdvection_invariantPhi():
     nkts = [n+1+deg*(int(p)-1) for (n, p) in zip(npts, periodic)]
     breaks = [np.linspace(*lims, num=num) for (lims, num) in zip(domain, nkts)]
     knots = [spl.make_knots(b, deg, p) for b, p in zip(breaks, periodic)]
-    bsplines = [spl.BSplines(k, deg, p) for k, p in zip(knots, periodic)]
+    bsplines = [spl.BSplines(k, deg, p, True) for k, p in zip(knots, periodic)]
     eta_grids = [bspl.greville for bspl in bsplines]
 
     eta_vals[0] = eta_grids[0]
@@ -167,7 +167,7 @@ def test_poloidalAdvection_vortex():
     N = 200
     dt = 0.1
 
-    v = 0
+    v = 0.0
 
     f_vals = np.ndarray([N+1, npts[1], npts[0]])
 
@@ -178,7 +178,7 @@ def test_poloidalAdvection_vortex():
     nkts = [n+1+deg*(int(p)-1) for (n, p) in zip(npts, periodic)]
     breaks = [np.linspace(*lims, num=num) for (lims, num) in zip(domain, nkts)]
     knots = [spl.make_knots(b, deg, p) for b, p in zip(breaks, periodic)]
-    bsplines = [spl.BSplines(k, deg, p) for k, p in zip(knots, periodic)]
+    bsplines = [spl.BSplines(k, deg, p, True) for k, p in zip(knots, periodic)]
     eta_grids = [bspl.greville for bspl in bsplines]
 
     eta_vals[0] = eta_grids[0]
@@ -243,7 +243,7 @@ def test_poloidalAdvection_constantAdv():
     N = 200
     dt = 0.1
 
-    v = 0
+    v = 0.0
 
     f_vals = np.ndarray([N+1, npts[1], npts[0]])
 
@@ -254,7 +254,7 @@ def test_poloidalAdvection_constantAdv():
     nkts = [n+1+deg*(int(p)-1) for (n, p) in zip(npts, periodic)]
     breaks = [np.linspace(*lims, num=num) for (lims, num) in zip(domain, nkts)]
     knots = [spl.make_knots(b, deg, p) for b, p in zip(breaks, periodic)]
-    bsplines = [spl.BSplines(k, deg, p) for k, p in zip(knots, periodic)]
+    bsplines = [spl.BSplines(k, deg, p, True) for k, p in zip(knots, periodic)]
     eta_grids = [bspl.greville for bspl in bsplines]
 
     eta_vals[0] = eta_grids[0]
@@ -393,7 +393,7 @@ def test_poloidalAdvection():
     N = 100
     dt = 0.01
 
-    v = 0
+    v = 0.0
 
     f_vals = np.ndarray([N+1, npts[1]+1, npts[0]])
     # ~ f_vals = np.ndarray([N+1,npts[1],npts[0]])
@@ -405,7 +405,7 @@ def test_poloidalAdvection():
     nkts = [n+1+deg*(int(p)-1) for (n, p) in zip(npts, periodic)]
     breaks = [np.linspace(*lims, num=num) for (lims, num) in zip(domain, nkts)]
     knots = [spl.make_knots(b, deg, p) for b, p in zip(breaks, periodic)]
-    bsplines = [spl.BSplines(k, deg, p) for k, p in zip(knots, periodic)]
+    bsplines = [spl.BSplines(k, deg, p, True) for k, p in zip(knots, periodic)]
     eta_grids = [bspl.greville for bspl in bsplines]
 
     eta_vals[0] = eta_grids[0]
@@ -435,6 +435,8 @@ def test_poloidalAdvection():
     endPts[0][:] = polAdv._shapedQ + 2*a*dt/constants.B0
     endPts[1][:] = np.sqrt(polAdv._points[1]**2-c*np.sin(d*polAdv._shapedQ)/a/constants.B0
                            + c*np.sin(d*endPts[0])/a/constants.B0)
+
+    polAdv.allow_tests()
 
     for n in range(N):
         f_vals[n+1, :-1, :] = f_vals[n, :-1, :]
@@ -516,7 +518,7 @@ def test_fluxAdvection_dz():
 
     N = 100
 
-    v = 0
+    v = 0.0
 
     eta_vals = [np.linspace(0, 1, 4), np.linspace(0, 2*pi, npts[0], endpoint=False),
                 np.linspace(0, 20, npts[1], endpoint=False), np.linspace(0, 1, 4)]
@@ -529,7 +531,7 @@ def test_fluxAdvection_dz():
     nkts = [n+1 for n in npts]
     breaks = [np.linspace(*lims, num=num) for (lims, num) in zip(domain, nkts)]
     knots = [spl.make_knots(b, 3, True) for b in breaks]
-    bsplines = [spl.BSplines(k, 3, True) for k in knots]
+    bsplines = [spl.BSplines(k, 3, True, True) for k in knots]
     eta_grids = [bspl.greville for bspl in bsplines]
 
     eta_vals[1] = eta_grids[0]
@@ -601,7 +603,7 @@ def test_flux_aligned():
     nkts = [n+1 for n in npts]
     breaks = [np.linspace(*lims, num=num) for (lims, num) in zip(domain, nkts)]
     knots = [spl.make_knots(b, 3, True) for b in breaks]
-    bsplines = [spl.BSplines(k, 3, True) for k in knots]
+    bsplines = [spl.BSplines(k, 3, True, True) for k in knots]
     eta_grids = [bspl.greville for bspl in bsplines]
 
     eta_vals[1] = eta_grids[0]
@@ -703,9 +705,9 @@ def test_Phi_deriv():
     constants = get_constants('testSetups/iota8.json')
 
     breaks_theta = np.linspace(0, 2*pi, npts[1]+1)
-    spline_theta = spl.BSplines(spl.make_knots(breaks_theta, 3, True), 3, True)
+    spline_theta = spl.BSplines(spl.make_knots(breaks_theta, 3, True), 3, True, True)
     breaks_z = np.linspace(0, 20, npts[2]+1)
-    spline_z = spl.BSplines(spl.make_knots(breaks_z, 3, True), 3, True)
+    spline_z = spl.BSplines(spl.make_knots(breaks_z, 3, True), 3, True, True)
 
     eta_grid = [np.array([1]), spline_theta.greville, spline_z.greville]
 
