064be40438a31d928f17701eabcaee7991948942
Emily Bourne <emily.bourne@epfl.ch>
2025-09-24 20:37:47 +0200

    Use low-level calls where possible

--- pygyro/advection/accelerated_advection_steps.py
+++ pygyro/advection/accelerated_advection_steps.py
@@ -1,6 +1,9 @@
+import numpy as np
 from typing import Final
 from pyccel.decorators import pure
 from ..splines.splines import Spline1D, Spline2D
+from ..splines.accelerated_spline_interpolators import solve_system_nonperiodic, solve_system_periodic, solve_2d_system
+from ..splines.sll_m_spline_matrix_periodic_banded import PeriodicBandedMatrix
 from ..initialisation.initialiser_funcs import f_eq
 
 
@@ -37,6 +40,7 @@ def poloidal_advection_step_expl(f: 'float[:,:]',
 
     multFactor = dt / B0
     multFactor_half = 0.5 * multFactor
+
     phi_spline.eval_vector(qPts, rPts, drPhi_0, 0, 1)
     phi_spline.eval_vector(qPts, rPts, dthetaPhi_0, 1, 0)
 
@@ -141,6 +145,24 @@ def v_parallel_advection_eval_step(f: 'float[:]', vPts: 'float[:]',
             f[i] = spl.eval(v)
 
 
+def v_parallel_advection_eval_step_loop(f: 'float[:,:,:]', vPts: 'float[:]',
+                                           rPos: 'float', vMin: 'float', vMax: 'float',
+                                        spl: Spline1D, bmat: 'float[:,:](order=F)', l: np.int32, u: np.int32, ipiv: 'int32[:]',
+                                        parGradVals: 'float[:,:,:]', dt: float, i: int,
+                                           CN0: 'float', kN0: 'float', deltaRN0: 'float', rp: 'float',
+                                           CTi: 'float', kTi: 'float', deltaRTi: 'float', bound: 'int'):
+    n1, n2, _ = f.shape
+    for j in range(n1):  # z
+        for k in range(n2):  # q
+            coeffs = spl.coeffs
+            solve_system_nonperiodic(f[j, k, :], coeffs, bmat, l, u, ipiv)
+
+            vPts -= parGradVals[i, j, k]*dt
+
+            v_parallel_advection_eval_step(f[j, k, :], vPts, rPos, vMin, vMax, spl,
+                                           CN0, kN0, deltaRN0, rp, CTi, kTi, deltaRTi, bound)
+
+
 def get_lagrange_vals(i: 'int', shifts: 'int[:]',
                       vals: 'float[:,:,:]', qVals: 'float[:]',
                       thetaShifts: 'float[:]', spl: Spline1D):
@@ -168,6 +190,28 @@ def flux_advection(nq: 'int', nr: 'int',
             f[j, i] = coeffs[0]*vals[i, j, 0]
             for k in range(1, len(coeffs)):
                 f[j, i] += coeffs[k]*vals[i, j, k]
+
+
+def flux_advection_loop(f: 'float[:,:,:,:]', thetaSpline: Spline1D, theta_offset: int, theta_splu: PeriodicBandedMatrix,
+                        shifts: 'int[:,:,:]', lagrange_vals: 'float[:,:,:]', qVals: 'float[:]',
+                        thetaShifts: 'float[:,:,:]', lagrange_coeffs: 'float[:,:,:]'):
+    nr, nv, nq, nz = f.shape
+
+    for rIdx in range(nr):  # r
+        for cIdx in range(nv):  # v
+            # find the values of the function at each required point
+            for k in range(nz):
+                solve_system_periodic(
+                    f[rIdx, cIdx, :, k], thetaSpline, theta_offset, theta_splu)
+
+                get_lagrange_vals(k, shifts[rIdx, cIdx],
+                                  lagrange_vals, qVals,
+                                  thetaShifts[rIdx, cIdx],
+                                  thetaSpline)
+
+            flux_advection(nq, nz, f[rIdx, cIdx],
+                           lagrange_coeffs[rIdx, cIdx],
+                           lagrange_vals)
 
 
 def poloidal_advection_step_impl(f: 'float[:,:]', dt: 'float', v: 'float', rPts: 'float[:]', qPts: 'float[:]',
@@ -294,3 +338,37 @@ def poloidal_advection_step_impl(f: 'float[:,:]', dt: 'float', v: 'float', rPts:
                     endPts_k2_q[i, j] = endPts_k2_q[i, j] % (2*pi)
                     f[i, j] = pol_spline.eval(
                         endPts_k2_q[i, j], endPts_k2_r[i, j])
+
+
+def poloidal_advection_loop(f: 'float[:,:,:,:]', phi: 'float[:,:,:]', dt: 'float',
+                            is_explicit: 'bool', rPts: 'float[:]', qPts: 'float[:]',
+                            vPts: 'float[:]', interp_wt: 'float[:,:]',
+                            r_bmat: 'float[:,:](order=F)', r_l: np.int32, r_u: np.int32,
+                            r_ipiv: 'int32[:]', theta_offset: int, theta_splu: PeriodicBandedMatrix,
+                            drPhi_0: 'float[:,:]', dthetaPhi_0: 'float[:,:]', drPhi_k: 'float[:,:]', dthetaPhi_k: 'float[:,:]',
+                            endPts_k1_q: 'float[:,:]', endPts_k1_r: 'float[:,:]', endPts_k2_q: 'float[:,:]', endPts_k2_r: 'float[:,:]',
+                            phi_spline: Spline2D, pol_spline: Spline2D,
+                            CN0: 'float', kN0: 'float', deltaRN0: 'float', rp: 'float', CTi: 'float', kTi: 'float', deltaRTi: 'float',
+                            B0: 'float', tol: 'float', nulBound: 'bool'):
+    nv, nz, _, _ = f.shape
+    for j in range(nz):
+        solve_2d_system(phi[j], phi_spline, interp_wt, r_bmat, r_l, r_u,
+                        r_ipiv, theta_offset, theta_splu)
+
+        # Do step
+        for i, v in enumerate(vPts):
+            solve_2d_system(f[i, j], pol_spline, interp_wt, r_bmat, r_l, r_u,
+                            r_ipiv, theta_offset, theta_splu)
+
+            if is_explicit:
+                poloidal_advection_step_expl(f[i, j], float(dt), v, rPts, qPts,
+                                             drPhi_0, dthetaPhi_0, drPhi_k, dthetaPhi_k,
+                                             endPts_k1_q, endPts_k1_r, endPts_k2_q, endPts_k2_r,
+                                             phi_spline, pol_spline, CN0, kN0, deltaRN0, rp, CTi,
+                                             kTi, deltaRTi, B0, nulBound)
+            else:
+                poloidal_advection_step_impl(f[i, j], float(dt), v, rPts, qPts,
+                                             drPhi_0, dthetaPhi_0, drPhi_k, dthetaPhi_k,
+                                             endPts_k1_q, endPts_k1_r, endPts_k2_q, endPts_k2_r,
+                                             phi_spline, pol_spline, CN0, kN0, deltaRN0, rp, CTi,
+                                             kTi, deltaRTi, B0, tol, nulBound)
--- pygyro/splines/accelerated_spline_interpolators.py
+++ pygyro/splines/accelerated_spline_interpolators.py
@@ -1,7 +1,7 @@
 from typing import TypeVar, Final
 import numpy as np
 from pyccel.stdlib.internal.lapack import dgbtrs, zgbtrs
-from .splines import Spline1D, BSplines
+from .splines import Spline1D, Spline2D, BSplines
 from .sll_m_spline_matrix_periodic_banded import PeriodicBandedMatrix
 
 T = TypeVar('T', float, complex)
@@ -48,3 +48,55 @@ def solve_system_nonperiodic(ug: 'Final[T[:]]', c: 'T[:]', bmat: 'Final[T[:,:](o
             bmat.shape[0]), ipiv, c, np.int32(c.shape[0]), sinfo)
 
     return sinfo
+
+
+def solve_2d_system(ug: 'float[:,:]', spl: Spline2D, wt: 'float[:,:]',
+                    r_bmat: 'float[:,:](order=F)', r_l: np.int32, r_u: np.int32, r_ipiv: 'int32[:]',
+                    theta_offset: int, theta_splu: PeriodicBandedMatrix):
+    basis1 = spl.basis1
+    basis2 = spl.basis2
+    n1, n2 = basis1.nbasis, basis2.nbasis
+    p1, p2 = basis1.degree, basis2.degree
+    assert ug.shape[0] == n1
+    assert ug.shape[1] == n2
+
+    spline1 = Spline1D(basis1)
+
+    w = spl.coeffs
+
+    s1, s2 = w.shape
+
+    # Cycle over x1 position and interpolate f along x2 direction.
+    # Work on spl.coeffs
+    # $ omp parallel for
+    # for i1 in range(n1):
+    #    solve_system_nonperiodic(ug[i1, :], w[i1, :], r_bmat, r_l, r_u, r_ipiv)
+    sinfo: np.int32
+    dgbtrs('N', np.int32(r_bmat.shape[1]), r_l, r_u, np.int32(
+        n1), r_bmat, np.int32(r_bmat.shape[0]), r_ipiv, w.T, np.int32(s2), sinfo)
+
+    # Transpose coefficients to self._bwork
+    # $ omp parallel for collapse(2)
+    for i1 in range(s1):
+        for i2 in range(s2):
+            wt[i2, i1] = w[i1, i2]
+
+    # Cycle over x2 position and interpolate w along x1 direction.
+    # Work on self._bwork
+    # $ omp parallel for firstprivate(spline1) private(c)
+    for i2 in range(n2):
+        solve_system_periodic(wt[i2, :n1], spline1, theta_offset, theta_splu)
+        # self._interp1.compute_interpolant(wt[i2, :n1], self._spline1)
+        c = spline1.coeffs
+        wt[i2, :] = c
+
+    # Transpose coefficients to spl.coeffs
+    # $ omp parallel for collapse(2)
+    for i1 in range(s1):
+        for i2 in range(s2):
+            w[i1, i2] = wt[i2, i1]
+
+    # x1-periodic only: "wrap around" coefficients onto extended array
+    for i1 in range(p1):
+        for i2 in range(s2):
+            w[n1 + i1, i2] = w[i1, i2]
